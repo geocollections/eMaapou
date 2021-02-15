236cd63a5b28957981a4ebf02c483d2eec27ae3a
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-02-15 10:24:01 +0200

    getResourceList depends on Vuetify Data Table options Fixes #90

--- services/sarvREST.api.js
+++ services/sarvREST.api.js
@@ -1,43 +1,55 @@
 import { isEmpty } from 'lodash'
 
-export default ($axios) => ({
-  async getResource(resource, id) {
-    const response = await $axios.get(`${resource}/${id}`)
-    return response.data
-  },
-  // ISSUE: getResourceList depends on Vuetify Data Table options
-  async getResourceList(
-    resource,
-    { defaultParams, queryFields, search, ...options }
-  ) {
-    if (options.isValid) {
-      return { items: [], count: 0 }
-    }
-
-    let multiSearch
-    if (!isEmpty(options.search))
-      multiSearch = `value:${search};fields:${Object.values(queryFields)
-        .map((field) => field())
-        .join()};lookuptype:icontains`
-
-    let params = {
-      ...defaultParams,
-      multi_search: multiSearch,
+const getPaginationParams = (options) => {
+  if (options?.page && options?.itemsPerPage) {
+    return {
       paginate_by: options.itemsPerPage,
       page: options.page,
     }
+  }
+  return null
+}
 
+const getSortByParams = (options, queryFields) => {
+  if (options?.sortBy && options?.sortDesc) {
     if (!isEmpty(options.sortBy)) {
       const orderBy = options.sortBy.map((field, i) => {
         if (options.sortDesc[i]) return `-${queryFields[field]()}`
         return queryFields[field]()
       })
 
-      params = {
-        ...params,
-        order_by: orderBy.join(','),
-      }
+      return { order_by: orderBy.join(',') }
     }
+  }
+  return null
+}
+
+export default ($axios) => ({
+  async getResource(resource, id) {
+    const response = await $axios.get(`${resource}/${id}`)
+    return response.data
+  },
+  async getResourceList(
+    resource,
+    { defaultParams, queryFields, search, tableOptions, isValid, ...options }
+  ) {
+    if (isValid) {
+      return { items: [], count: 0 }
+    }
+
+    let multiSearch
+    if (!isEmpty(search))
+      multiSearch = `value:${search};fields:${Object.values(queryFields)
+        .map((field) => field())
+        .join()};lookuptype:icontains`
+
+    const params = {
+      ...defaultParams,
+      multi_search: multiSearch,
+      ...getPaginationParams(tableOptions),
+      ...getSortByParams(tableOptions, queryFields),
+    }
+
     const response = await $axios.$get(resource, { params })
 
     return {
--- services/sarvSolr.api.js
+++ services/sarvSolr.api.js
@@ -1,34 +1,48 @@
 import { isEmpty } from 'lodash'
 
-export default ($axios) => ({
-  async getResourceList(
-    resource,
-    { defaultParams, queryFields, search, ...options }
-  ) {
-    if (options.isValid) {
-      return {
-        items: [],
-        count: 0,
-      }
-    }
-    let params = {
-      ...defaultParams,
-      q: isEmpty(search) ? '*' : `${search}`,
+const getPaginationParams = (options) => {
+  if (options?.page && options?.itemsPerPage) {
+    return {
       start: (options.page - 1) * options.itemsPerPage,
       rows: options.itemsPerPage,
     }
+  }
+  return null
+}
 
+const getSortByParams = (options, queryFields) => {
+  if (options?.sortBy && options?.sortDesc) {
     if (!isEmpty(options.sortBy)) {
       const orderBy = options.sortBy.map((field, i) => {
         if (options.sortDesc[i]) return `${queryFields[field]()} desc`
         return `${queryFields[field]()} asc`
       })
 
-      params = {
-        ...params,
-        sort: orderBy.join(','),
+      return { sort: orderBy.join(',') }
+    }
+  }
+  return null
+}
+
+export default ($axios) => ({
+  async getResourceList(
+    resource,
+    { defaultParams, queryFields, search, tableOptions, isValid, ...options }
+  ) {
+    if (isValid) {
+      return {
+        items: [],
+        count: 0,
       }
     }
+
+    const params = {
+      ...defaultParams,
+      q: isEmpty(search) ? '*' : `${search}`,
+      ...getPaginationParams(tableOptions),
+      ...getSortByParams(tableOptions, queryFields),
+    }
+
     const response = await $axios.$get(`solr/${resource}`, { params })
 
     return {
