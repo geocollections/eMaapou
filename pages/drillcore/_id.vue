3b5ed6e47bf6f05d980a6aa13699a1d01805f7f5
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-02-18 16:20:50 +0200

    Use DataRow components

--- pages/drillcore/_id.vue
+++ pages/drillcore/_id.vue
@@ -22,113 +22,52 @@
                 <v-simple-table dense class="custom-table">
                   <template #default>
                     <tbody>
-                      <tr>
-                        <td>{{ $t('drillcore.name') }}</td>
-                        <td>
-                          {{
-                            $translate({
-                              et: drillcore.drillcore,
-                              en: drillcore.drillcore_en,
-                            })
-                          }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('drillcore.boxes') }}</td>
-                        <td v-if="isNull(drillcore.boxes)" class="no-value">
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.boxes }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('drillcore.boxNumbers') }}</td>
-                        <td
-                          v-if="isEmpty(drillcore.box_numbers)"
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.box_numbers }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('drillcore.depository') }}</td>
-                        <td
-                          v-if="
-                            isEmpty(
-                              $translate({
-                                et: drillcore.depository__value,
-                                en: drillcore.depository__value_en,
-                              })
-                            )
-                          "
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{
-                            $translate({
-                              et: drillcore.depository__value,
-                              en: drillcore.depository__value_en,
-                            })
-                          }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('drillcore.storage') }}</td>
-                        <td
-                          v-if="isEmpty(drillcore.storage__location)"
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.storage__location }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('drillcore.driller') }}</td>
-                        <td
-                          v-if="isEmpty(drillcore.agent__agent)"
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.agent__agent }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('drillcore.year') }}</td>
-
-                        <td v-if="isEmpty(drillcore.year)" class="no-value">
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.year }}
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td>{{ $t('drillcore.metersInBox') }}</td>
-                        <td
-                          v-if="isNull(drillcore.number_meters)"
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.number_meters }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('drillcore.id') }}</td>
-                        <td>{{ drillcore.id }}</td>
-                      </tr>
+                      <data-row
+                        :title="$t('drillcore.name')"
+                        :value="
+                          $translate({
+                            et: drillcore.drillcore,
+                            en: drillcore.drillcore_en,
+                          })
+                        "
+                      />
+                      <data-row
+                        :title="$t('drillcore.boxes')"
+                        :value="drillcore.boxes"
+                      />
+                      <data-row
+                        :title="$t('drillcore.boxNumbers')"
+                        :value="drillcore.box_numbers"
+                      />
+                      <data-row
+                        :title="$t('drillcore.depository')"
+                        :value="
+                          $translate({
+                            et: drillcore.depository__value,
+                            en: drillcore.depository__value_en,
+                          })
+                        "
+                      />
+                      <data-row
+                        :title="$t('drillcore.storage')"
+                        :value="drillcore.storage__location"
+                      />
+                      <data-row
+                        :title="$t('drillcore.driller')"
+                        :value="drillcore.agent__agent"
+                      />
+                      <data-row
+                        :title="$t('drillcore.year')"
+                        :value="drillcore.year"
+                      />
+                      <data-row
+                        :title="$t('drillcore.metersInBox')"
+                        :value="drillcore.number_meters"
+                      />
+                      <data-row
+                        :title="$t('drillcore.id')"
+                        :value="drillcore.id"
+                      />
                     </tbody>
                   </template>
                 </v-simple-table>
@@ -155,103 +94,46 @@
                 <v-simple-table dense class="mb-4 custom-table">
                   <template #default>
                     <tbody>
-                      <tr>
-                        <td>{{ $t('locality.locality') }}</td>
-                        <td>
-                          <a
-                            class="text-link"
-                            :href="
-                              localePath({
-                                name: 'locality-id',
-                                params: { id: drillcore.locality_id },
-                              })
-                            "
-                          >
-                            {{
-                              $translate({
-                                et: drillcore.locality__locality,
-                                en: drillcore.locality__locality_en,
-                              })
-                            }}
-                          </a>
-                        </td>
-                      </tr>
-                      <tr v-if="drillcore.locality__country__value">
-                        <td>{{ $t('locality.country') }}</td>
-                        <td
-                          v-if="
-                            isEmpty(
-                              $translate({
-                                et: drillcore.locality__country__value,
-                                en: drillcore.locality__country__value_en,
-                              })
-                            )
-                          "
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{
-                            $t('locality.countryFormat', {
-                              name: $translate({
-                                et: drillcore.locality__country__value,
-                                en: drillcore.locality__country__value_en,
-                              }),
-                              iso: drillcore.locality__country__iso_code,
-                            })
-                          }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('locality.latitude') }}</td>
-                        <td
-                          v-if="isNull(drillcore.locality__latitude)"
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.locality__latitude }}
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td>{{ $t('locality.longitude') }}</td>
-                        <td
-                          v-if="isNull(drillcore.locality__longitude)"
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.locality__longitude }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('locality.elevation') }}</td>
-                        <td
-                          v-if="isNull(drillcore.locality__elevation)"
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.locality__elevation }}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>{{ $t('locality.depth') }}</td>
-                        <td
-                          v-if="isNull(drillcore.locality__depth)"
-                          class="no-value"
-                        >
-                          {{ $t('common.noValue') }}
-                        </td>
-                        <td v-else>
-                          {{ drillcore.locality__depth }}
-                        </td>
-                      </tr>
+                      <link-data-row
+                        :title="$t('locality.locality')"
+                        :value="
+                          $translate({
+                            et: drillcore.locality__locality,
+                            en: drillcore.locality__locality_en,
+                          })
+                        "
+                        :href="
+                          localePath({
+                            name: 'locality-id',
+                            params: { id: drillcore.locality_id },
+                          })
+                        "
+                      />
+                      <data-row
+                        :title="$t('locality.country')"
+                        :value="
+                          $translate({
+                            et: drillcore.locality__country__value,
+                            en: drillcore.locality__country__value_en,
+                          })
+                        "
+                      />
+                      <data-row
+                        :title="$t('locality.latitude')"
+                        :value="drillcore.locality__latitude"
+                      />
+                      <data-row
+                        :title="$t('locality.longitude')"
+                        :value="drillcore.locality__longitude"
+                      />
+                      <data-row
+                        :title="$t('locality.elevation')"
+                        :value="drillcore.locality__elevation"
+                      />
+                      <data-row
+                        :title="$t('locality.depth')"
+                        :value="drillcore.locality__depth"
+                      />
                     </tbody>
                   </template>
                 </v-simple-table>
@@ -301,9 +183,11 @@
 import { isEmpty, isNull } from 'lodash'
 import LeafletMap from '@/components/LeafletMap'
 import Tabs from '~/components/Tabs.vue'
+import DataRow from '~/components/DataRow.vue'
+import LinkDataRow from '~/components/LinkDataRow.vue'
 
 export default {
-  components: { Tabs, LeafletMap },
+  components: { Tabs, LeafletMap, DataRow, LinkDataRow },
   layout: 'detail',
   async asyncData({ params, route, error, app }) {
     try {
