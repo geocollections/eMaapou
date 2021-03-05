fbb134e261d00cd0007f468a91f7c44c862e9ba7
ollehints <olle.hints@ttu.ee>
2021-03-05 15:53:02 +0200

    small updates and translations

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,15 +5,20 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s | EMA',
-    title: 'EMA',
+    titleTemplate: '%s | e·Maapõu',
+    title: 'e·Maapõu',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'e·Maapõu is web portal for geoscience data from Estonia. It provides direct saerch capabilities to information system SARV and links to other relevant data sources',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
