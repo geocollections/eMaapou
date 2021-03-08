7cb9a4d40a46e740263d5084803acb5f8bfb5984
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-03-08 15:40:20 +0200

    Search drawer #140 add site search

--- pages/search/sites.vue
+++ pages/search/sites.vue
@@ -2,7 +2,7 @@
   <external-search-table-wrapper
     :external-search="search"
     :items="items"
-    :headers="headers"
+    :headers="translatedHeaders"
     :count="count"
     :init-options="options"
     @update="handleUpdate"
@@ -19,36 +19,21 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import ExternalSearchTableWrapper from '@/components/tables/ExternalSearchTableWrapper'
 export default {
   components: { ExternalSearchTableWrapper },
-  data() {
-    return {
-      items: [],
-      count: 0,
-      options: {
-        page: 1,
-        itemsPerPage: 25,
-        sortBy: ['id'],
-        sortDesc: [true],
-      },
-      headers: [
-        { text: this.$t('site.id'), value: 'id' },
-        { text: this.$t('site.name'), value: 'name' },
-        { text: this.$t('site.latitude'), value: 'latitude' },
-        { text: this.$t('site.longitude'), value: 'longitude' },
-      ],
-      queryFields: {
-        id: () => 'id_numeric',
-        name: () => 'name',
-        project: () =>
-          this.$i18n.locale === 'et' ? 'project__name' : 'project__name_en',
-      },
-    }
-  },
   computed: {
     ...mapState('landing', ['search']),
+    ...mapState('site', ['options', 'items', 'count', 'headers']),
+    translatedHeaders() {
+      return this.headers.map((header) => {
+        return {
+          ...header,
+          text: this.$t(header.text),
+        }
+      })
+    },
   },
   created() {
     this.$store.subscribe((mutation, _) => {
@@ -58,18 +43,9 @@ export default {
     })
   },
   methods: {
+    ...mapActions('site', ['searchSites']),
     async handleUpdate(options) {
-      const siteResponse = await this.$services.sarvSolr.getResourceList(
-        'site',
-        {
-          ...options,
-          search: this.search,
-          queryFields: this.queryFields,
-        }
-      )
-      this.options = options
-      this.items = siteResponse.items
-      this.count = siteResponse.count
+      await this.searchSites(options.tableOptions)
     },
   },
 }
---
+++ store/site.js
@@ -0,0 +1,102 @@
+import { getField, updateField } from 'vuex-map-fields'
+
+const getDefaultState = () => {
+  return {
+    items: [],
+    count: 0,
+    options: {
+      page: 1,
+      itemsPerPage: 25,
+      sortBy: ['id'],
+      sortDesc: [true],
+    },
+    headers: [
+      { text: 'site.id', value: 'id' },
+      { text: 'site.name', value: 'name' },
+      { text: 'site.latitude', value: 'latitude' },
+      { text: 'site.longitude', value: 'longitude' },
+    ],
+    search: {
+      byIds: {
+        name: {
+          value: '',
+          type: 'text',
+          lookUpType: 'contains',
+          label: 'site.name',
+          fields: ['name'],
+        },
+        latitude: {
+          type: 'range',
+          lookUpType: 'range',
+          value: [-90, 90],
+          label: 'site.latitude',
+          placeholders: ['coorinates.min', 'coorinates.max'],
+          fields: ['latitude'],
+        },
+        longitude: {
+          type: 'range',
+          lookUpType: 'range',
+          value: [-180, 180],
+          label: 'site.longitude',
+          placeholders: ['coorinates.min', 'coorinates.max'],
+          fields: ['longitude'],
+        },
+      },
+      allIds: ['name'],
+    },
+  }
+}
+
+export const state = () => getDefaultState()
+
+export const getters = {
+  getField,
+}
+
+export const mutations = {
+  updateField,
+  SET_ITEMS(state, items) {
+    state.items = items
+  },
+  SET_COUNT(state, count) {
+    state.count = count
+  },
+  SET_OPTIONS(state, options) {
+    state.options = options
+  },
+  RESET_SEARCH(state) {
+    const defaultState = getDefaultState()
+
+    state.search = defaultState.search
+    state.options = { ...state.options, page: defaultState.options.page }
+  },
+}
+
+export const actions = {
+  resetSiteSearch({ commit }) {
+    commit('RESET_SEARCH')
+  },
+  async searchSites(
+    { commit, rootState, state },
+    options = { ...state.options, page: 1 }
+  ) {
+    commit('SET_OPTIONS', options)
+    // TODO: move these functions somewhere where they are not created every time the action is called
+    // TODO: Check is these are even used
+    const queryFields = {
+      id: () => 'id_numeric',
+      name: () => 'name',
+      project: () =>
+        this.$i18n.locale === 'et' ? 'project__name' : 'project__name_en',
+    }
+
+    const siteResponse = await this.$services.sarvSolr.getResourceList('site', {
+      tableOptions: options,
+      search: rootState.landing.search,
+      queryFields,
+      searchFilters: state.search.byIds,
+    })
+    commit('SET_ITEMS', siteResponse.items)
+    commit('SET_COUNT', siteResponse.count)
+  },
+}
