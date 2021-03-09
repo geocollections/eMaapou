e4b784627b47e03e50d75ab025c30c81d04b94dc
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-03-09 16:28:49 +0200

    add simple transition animation to button tabs

--- components/ButtonTabs.vue
+++ components/ButtonTabs.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
     <v-chip-group column active-class="active deep-orange--text text--darken-2">
-      <v-chip
-        v-for="(item, index) in tabs"
-        :key="index"
-        class="mx-2 mb-3"
-        :disabled="item.count === 0"
-        nuxt
-        rounded
-        exact
-        :to="
-          localePath({
-            name: item.routeName,
-            params: { id: $route.params.id },
-          })
-        "
-        @click="activeTab = localePath({ name: item.routeName })"
-      >
-        {{ $t(item.title, { number: item.count }) }}
-      </v-chip>
+      <transition-group name="flip-list">
+        <v-chip
+          v-for="(item, index) in tabs"
+          :key="index"
+          class="mx-2 mb-3"
+          :disabled="item.count === 0"
+          nuxt
+          rounded
+          exact
+          :to="
+            localePath({
+              name: item.routeName,
+              params: { id: $route.params.id },
+            })
+          "
+          @click="activeTab = localePath({ name: item.routeName })"
+        >
+          {{ $t(item.title, { number: item.count }) }}
+        </v-chip>
+      </transition-group>
     </v-chip-group>
     <v-card>
       <v-tabs-items v-model="activeTab" touchless>
@@ -69,5 +71,10 @@ export default {
 
 .active {
   font-weight: bold;
+}
+
+.flip-list-move {
+  transition: all 1s ease;
+  -webkit-transition: all 1s ease;
 }
 </style>
