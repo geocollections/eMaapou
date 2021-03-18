99dc11281ac84fa34aebb8c4f3359f0e5c122860
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-03-18 09:51:54 +0200

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~plugins/translate.js',
-    { src: '~/plugins/infiniteLoading', ssr: false },
+    { src: '~/plugins/infiniteLoading.js', mode: 'client' },
     '~plugins/services.js',
     { src: '~plugins/leaflet.js', mode: 'client' },
     '~plugins/openWindow.js',
