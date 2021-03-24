7fc427443404a8e30dd85cfa2a90264a3d2f2b91
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-03-24 09:16:32 +0200

    #231 flip the list and remove duplicates

--- store/history.js
+++ store/history.js
@@ -8,14 +8,16 @@ export const state = () => {
 
 export const mutations = {
   PUSH_HISTORY(state, historyObject) {
-    if (state.history.length >= HISTORY_SIZE) state.history.shift()
+    if (state.history[0]?.to === historyObject.to) return
+
+    if (state.history.length >= HISTORY_SIZE) state.history.pop()
 
     if (state.history.length > 0) {
-      const lastObject = state.history.pop()
-      state.history.push({ ...lastObject, disabled: false })
+      const lastObject = state.history.shift()
+      state.history.unshift({ ...lastObject, disabled: false })
     }
 
-    state.history.push({ ...historyObject, disabled: true })
+    state.history.unshift({ ...historyObject, disabled: true })
   },
   SET_HISTORY(state, history) {
     state.history = history
