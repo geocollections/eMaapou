e5e1320494b42a6a8c3625c25e9a3c16a4d1826f
Enar Mustonen <ents100@gmail.com>
2021-04-20 09:48:29 +0300

    Closes #337 now clustering at 250 and above

---
+++ components/map/VMarkerClusterWrapper.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-marker-cluster
+    :options="{ spiderfyOnMaxZoom: false, disableClusteringAtZoom: 10 }"
+  >
+    <l-circle-marker-wrapper :markers="markers" />
+  </v-marker-cluster>
+</template>
+
+<script>
+import LCircleMarkerWrapper from '~/components/map/LCircleMarkerWrapper'
+export default {
+  name: 'VMarkerClusterWrapper',
+  components: { LCircleMarkerWrapper },
+  props: {
+    markers: {
+      type: Array,
+      required: false,
+      default() {
+        return []
+      },
+    },
+  },
+}
+</script>
+
+<style scoped></style>
