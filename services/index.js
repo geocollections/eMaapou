fe8c0c2d3e270494e3080f85233c04216bd8700b
Enar Mustonen <ents100@gmail.com>
2021-04-26 15:50:30 +0300

    #353 added site_summary layer + click event

--- services/index.js
+++ services/index.js
@@ -1,9 +1,11 @@
 import createSarvRESTRepository from '~/services/sarvREST.api'
 import createSarvSolrRepository from '~/services/sarvSolr.api'
 import createSarvRwapiRepository from '~/services/sarvRwapi.api'
+import createGeoserverRepository from '~/services/geoserver.api'
 
 export default (context) => ({
   sarvREST: createSarvRESTRepository(context.$axios),
   sarvSolr: createSarvSolrRepository(context.$axios),
   sarvRwapi: createSarvRwapiRepository(context.$axios),
+  geoserver: createGeoserverRepository(context.$axios),
 })
