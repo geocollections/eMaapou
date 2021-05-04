bc8bd9208420165a592c3542f1e802905325e6cc
Enar Mustonen <ents100@gmail.com>
2021-05-04 19:24:44 +0300

    #388 update

--- lang/et.js
+++ lang/et.js
@@ -372,7 +372,7 @@ export default {
     taxa: 'Taksonid ({number})',
     sampleReferences: 'Kirjandus ({number})',
     analysisResults: 'Analüüsitulemused ({number})',
-    analyticalData: 'Statistilised graafikud',
+    analyticalData: 'Graafikud',
     sample: 'Proov',
     hierarchy: 'Stratigraafia',
     dateAdded: 'Kirje lisatud',
@@ -769,5 +769,10 @@ export default {
     columns: 'Veerud',
     parameter: 'Parameeter',
     analysisMethod: 'Meetod',
+  },
+  statistics: {
+    analysisMethods: 'Analüüsimeetodid',
+    activeParameters: 'Aktiivsed parameetrid',
+    activeParametersEmpty: 'Palun valige mõni parameeter, et andmeid kuvada.',
   },
 }
--- pages/locality/_id/statistics.vue
+++ pages/locality/_id/statistics.vue
@@ -1,5 +1,47 @@
 <template>
   <div class="pa-2">
+    <v-card v-if="!isLoading" flat tile>
+      <v-card-text class="pb-0">
+        <div class="text-body-1 font-weight-bold">
+          {{ $t('statistics.analysisMethods') }}
+        </div>
+        <div class="d-flex flex-row flex-wrap">
+          <v-checkbox
+            v-for="(entity, id) in Object.keys(methods)"
+            :key="id"
+            class="px-3 mt-0"
+            :label="entity"
+            hide-details
+            :indeterminate="isIndeterminate(methods[entity])"
+            :value="isCheckboxChecked(methods[entity])"
+            @change="handleCheckboxChange($event, methods[entity])"
+          />
+        </div>
+
+        <div class="text-body-1 font-weight-bold pt-4 pb-2">
+          {{ $t('statistics.activeParameters') }}
+        </div>
+        <v-select
+          v-model="selectedParameters"
+          class="parameters"
+          :items="parameters"
+          multiple
+          chips
+          deletable-chips
+          clearable
+          small-chips
+          outlined
+          :error-messages="
+            selectedParameters.length === 0
+              ? $t('statistics.activeParametersEmpty')
+              : ''
+          "
+        ></v-select>
+      </v-card-text>
+    </v-card>
+
+    <v-skeleton-loader v-else class="px-4" width="100%" type="paragraph" />
+
     <client-only>
       <chart-wrapper :options="lineChartOptions" @click="handleClick" />
     </client-only>
@@ -9,6 +51,7 @@
 <script>
 import { isNil } from 'lodash'
 import ChartWrapper from '~/components/ChartWrapper'
+
 export default {
   components: { ChartWrapper },
   props: {
@@ -23,6 +66,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       analysisResults: [],
       depth: [],
       count: 0,
@@ -34,9 +78,11 @@ export default {
       },
       parameters: [],
       selectedParameters: [],
+      methods: [],
     }
   },
   async fetch() {
+    this.isLoading = true
     if (this.analysisResults.length === 0) {
       const analysisResultsResponse = await this.$services.sarvSolr.getResourceList(
         'analysis_results',
@@ -65,7 +111,12 @@ export default {
             fl: 'depth',
             sort: 'depth asc, parameter asc',
             stats: 'on',
-            'stats.field': ['depth', 'parameter'],
+            'stats.field': [
+              'depth',
+              'parameter',
+              'analysis_method',
+              'analysis_method_en',
+            ],
             'stats.calcdistinct': true,
           },
         }
@@ -73,13 +124,29 @@ export default {
       this.depth = statsResponse?.stats?.stats_fields?.depth?.distinctValues
       this.parameters =
         statsResponse?.stats?.stats_fields?.parameter?.distinctValues
+      const methodKey =
+        this.$i18n.locale === 'en' ? 'analysis_method_en' : 'analysis_method'
 
-      this.selectedParameters = [
-        this.parameters[0],
-        this.parameters[1],
-        this.parameters[2],
-      ]
+      this.methods =
+        statsResponse?.stats?.stats_fields?.[methodKey]?.distinctValues
+      this.methods = this.methods.reduce((prev, curr) => {
+        prev[curr] = [
+          ...new Set(
+            this.analysisResults
+              .filter((item) => item[methodKey] === curr)
+              .map((item) => item.parameter)
+          ),
+        ]
+        return prev
+      }, {})
+      console.log(this.methods)
+
+      this.selectedParameters = this.parameters.slice(
+        0,
+        this.parameters.length > 3 ? 3 : this.parameters.length
+      )
     }
+    this.isLoading = false
   },
   computed: {
     lineChartOptions() {
@@ -89,6 +156,8 @@ export default {
         this.parameters?.length > 0
       ) {
         return {
+          animation: false,
+
           title: {
             text: this.$translate({
               et: this?.localityObject?.locality,
@@ -97,92 +166,103 @@ export default {
             left: 'center',
             top: 45,
             textStyle: {
-              fontSize: 22,
+              fontSize: 24,
             },
           },
-          // visualMap: {
-          //   orient: 'vertical',
-          // },
+
           tooltip: {
-            trigger: 'axis',
+            trigger: 'item',
+            axisPointer: {
+              type: 'cross',
+            },
+            backgroundColor: 'rgba(255, 255, 255, 0.8)',
           },
+
           legend: {
             type: 'scroll',
             top: '90',
             padding: [5, 50],
-            data: this.parameters,
-            selected: this.selectedParameters,
+            data: this.selectedParameters,
+            selected: this.parameters.reduce((prev, curr) => {
+              prev[curr] = this.selectedParameters.includes(curr)
+              return prev
+            }, {}),
             animationDurationUpdate: 400,
+            // selectedMode: 'single',
           },
+
           grid: {
             show: true,
             top: 135,
+            bottom: 90,
             containLabel: true,
           },
+
           toolbox: {
-            // orient: 'vertical',
             right: 35,
             feature: {
               saveAsImage: {},
               restore: {},
               dataView: {},
               dataZoom: {},
-              magicType: {
-                type: ['line', 'bar', 'stack'],
-              },
               brush: {},
             },
           },
+
           brush: {
             toolbox: ['rect', 'polygon', 'lineX', 'lineY', 'keep', 'clear'],
           },
+
           dataZoom: [
             {
               type: 'slider',
               show: true,
               xAxisIndex: [0],
-              // start: 1,
-              // end: 35,
             },
             {
               type: 'slider',
               show: true,
               yAxisIndex: [0],
               left: '93%',
-              // start: 29,
-              // end: 36,
             },
             {
               type: 'inside',
               xAxisIndex: [0],
-              // start: 1,
-              // end: 35,
-            },
-            {
-              type: 'inside',
-              yAxisIndex: [0],
-              // start: 29,
-              // end: 36,
             },
           ],
+
           xAxis: {
-            type: 'category',
+            type: 'value',
             boundaryGap: false,
             data: this.depth,
+            name: 'Depth',
+            nameLocation: 'center',
+            nameTextStyle: {
+              fontWeight: 'bold',
+              fontSize: 16,
+            },
+            nameGap: 25,
+            splitNumber: 7,
           },
           yAxis: {
             type: 'value',
+            name: 'Value',
+            nameLocation: 'center',
+            nameTextStyle: {
+              fontWeight: 'bold',
+              fontSize: 16,
+            },
+            nameGap: 55,
           },
+
           series: this.parameters.map((item) => {
             return {
               name: item,
-              type: 'line',
-              stack: this.analysisResults.find(
-                (result) => result.parameter === item
-              ).analysis_method,
+              type: 'scatter',
               data: this.analysisResults
                 .filter((result) => result.parameter === item)
-                .map((t) => t.value),
+                .map((t) => [t.depth, t.value]),
+              symbolSize: 8,
             }
           }),
         }
@@ -190,6 +270,30 @@ export default {
     },
   },
   methods: {
+    isIndeterminate(params) {
+      if (!this.isCheckboxChecked(params))
+        return params.some((item) => this.selectedParameters.includes(item))
+      else return false
+    },
+
+    isCheckboxChecked(params) {
+      return params.every((item) => this.selectedParameters.includes(item))
+    },
+
+    handleCheckboxChange(bool, parameters) {
+      console.log(bool)
+      if (bool) {
+        // this.selectedParameters = parameters
+        let selected = [...this.selectedParameters]
+        selected = selected.concat(parameters)
+        this.selectedParameters = [...new Set(selected)]
+      } else {
+        this.selectedParameters = this.selectedParameters.filter(
+          (item) => !parameters.includes(item)
+        )
+      }
+    },
+
     handleClick(evt) {
       console.log(evt)
     },
