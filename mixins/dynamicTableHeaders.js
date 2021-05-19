a3f839c932485d0e497067877ebdac8348990ad9
Enar Mustonen <ents100@gmail.com>
2021-05-19 15:43:21 +0300

    #384 added dynamicTableHeaders.js mixin

---
+++ mixins/dynamicTableHeaders.js
@@ -0,0 +1,30 @@
+export default {
+  computed: {
+    tableName() {
+      let table = this.getRouteBaseName()
+      if (table === 'photo') table = 'attachment'
+      return table
+    },
+    stateModuleName() {
+      const table = this.getRouteBaseName()
+      return table
+    },
+  },
+  async fetch() {
+    const allFieldNames = await this.$services.sarvSolr.getAllFieldNames(
+      this.tableName,
+      {
+        q: '*:*',
+        wt: 'csv',
+        rows: 0,
+        facet: 'on',
+      }
+    )
+    if (allFieldNames?.fields) {
+      await this.$store.dispatch(`tableHeaders/setAllFieldNames`, {
+        module: this.stateModuleName,
+        fields: allFieldNames?.fields.trim().split(','),
+      })
+    }
+  },
+}
