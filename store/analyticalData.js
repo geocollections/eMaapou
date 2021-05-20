d15c9eb346c1d7592d8facb59eb39063739abf55
Enar Mustonen <ents100@gmail.com>
2021-05-20 18:15:11 +0300

    Bug fix: fixed distinctListParameters getter

--- store/analyticalData.js
+++ store/analyticalData.js
@@ -196,7 +196,8 @@ export const getters = {
   distinctListParameters: (state) => (mustSeeParam) => {
     if (state.listParameters && state.listParameters.length > 0) {
       const distinctList = state.listParameters.filter(
-        (param) => !state.activeListParameters.includes(param)
+        (param) =>
+          !state.activeListParameters.find((item) => item.id === param.id)
       )
       return [mustSeeParam, ...distinctList]
     } else return [mustSeeParam]
