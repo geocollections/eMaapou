9c9ad350a3dbbdc8ba2d502d842b87ba7586df72
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-06-30 13:20:45 +0300

    change color

--- components/search/SearchButton.vue
+++ components/search/SearchButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn type="submit" color="accent darken-1" dark class="px-3 montserrat">
+  <v-btn type="submit" color="warning" dark class="px-3 montserrat">
     {{ $t('common.searchCommand') }}
     <v-icon right>mdi-magnify</v-icon>
   </v-btn>
