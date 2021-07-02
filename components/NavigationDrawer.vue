9c397376f97c8b458620bf8b943b6d14aaaa736e
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-07-02 16:01:26 +0300

    fix route

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -151,7 +151,7 @@ export default {
           icon: 'mdi-chart-scatter-plot',
         },
         {
-          name: 'analytical_data',
+          name: 'analytical-data',
           lang: 'analyticalData',
           icon: 'mdi-chart-scatter-plot',
         },
