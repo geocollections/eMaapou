bb8fe3ef0d0be5cab5d00de57bbe64fa55bf9394
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-08-12 10:11:00 +0300

    #566 style news article view

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -1,12 +1,16 @@
 <template>
-  <v-row>
-    <v-col>
+  <v-row justify="center">
+    <v-col cols="12" md="8" lg="6">
+      <div class="text-right montserrat">{{ formatDate(news.date_added) }}</div>
       <title-card
         :title="$translate({ et: news.title_et, en: news.title_en })"
         class="title-border"
         style="border-color: var(--v-header-darken1)"
       />
-      <div v-html="$translate({ et: news.text_et, en: news.text_en })"></div>
+      <div
+        class="montserrat"
+        v-html="$translate({ et: news.text_et, en: news.text_en })"
+      />
     </v-col>
   </v-row>
 </template>
@@ -25,6 +29,11 @@ export default {
     return {
       news: newsResponse.results[0],
     }
+  },
+  methods: {
+    formatDate(isoDateTime) {
+      return new Date(isoDateTime).toISOString().split('T')[0]
+    },
   },
 }
 </script>
