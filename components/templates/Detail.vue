54aa6ce1120613ae42a194777ad08318f07ff63f
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-08-13 11:43:13 +0300

    add padding

--- components/templates/Detail.vue
+++ components/templates/Detail.vue
@@ -2,20 +2,22 @@
   <v-row>
     <v-col>
       <slot name="title" />
-      <slot name="top" />
-      <slot>
-        <v-card>
-          <v-row no-gutters justify="center">
-            <v-col v-if="$slots['column-left']" cols>
-              <slot name="column-left" />
-            </v-col>
-            <v-col v-if="$slots['column-right']" cols="12" md="6">
-              <slot name="column-right" />
-            </v-col>
-          </v-row>
-        </v-card>
-      </slot>
-      <slot name="bottom" />
+      <div class="px-0 px-sm-3">
+        <slot name="top" />
+        <slot>
+          <v-card>
+            <v-row no-gutters justify="center">
+              <v-col v-if="$slots['column-left']" cols>
+                <slot name="column-left" />
+              </v-col>
+              <v-col v-if="$slots['column-right']" cols="12" md="6">
+                <slot name="column-right" />
+              </v-col>
+            </v-row>
+          </v-card>
+        </slot>
+        <slot name="bottom" />
+      </div>
     </v-col>
   </v-row>
 </template>
