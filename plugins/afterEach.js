27172436d2eefd823237c790250c0900112de469
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-09-22 10:00:51 +0300

    Fix #630 check start of route name

--- plugins/afterEach.js
+++ plugins/afterEach.js
@@ -2,14 +2,7 @@ const EXCLUDED_PAGES = ['index', 'about', 'news', 'news-id', 'search']
 
 export default ({ app, store }, inject) => {
   app.router.afterEach((to, from) => {
-    if (
-      EXCLUDED_PAGES.some((page) => {
-        if (page === 'search') return from.path.startsWith(app.localePath(page))
-
-        return app.localePath(page) === from.path
-      })
-    )
-      return
+    if (EXCLUDED_PAGES.some((page) => from.name?.startsWith(page))) return
 
     const name = serializeName(from)
     const id = from.params?.id ?? null
