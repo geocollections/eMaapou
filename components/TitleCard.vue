96b645c44173306d5d1369244c57a23b14fee11e
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-10-07 11:39:52 +0300

    remove title icons on xs screens

--- components/TitleCard.vue
+++ components/TitleCard.vue
@@ -11,46 +11,13 @@
         style="word-break: normal"
         class="text-sm-h3 font-weight-bold text-h4 pt-0 px-0 px-sm-3"
       >
-        <v-icon class="pr-2" x-large>{{ icon }}</v-icon>
+        <v-icon v-show="$vuetify.breakpoint.smAndUp" class="pr-sm-2" large>
+          {{ icon }}
+        </v-icon>
         {{ title }}
       </v-card-title>
     </slot>
   </v-card>
-  <!-- NOTE: Old implementation -->
-  <!-- <v-card
-    class="mb-4 d-flex justify-space-between px-4 flex-column flex-sm-row"
-  >
-    <div
-      class="align-self-start align-self-sm-center text-no-wrap hidden-xs-only"
-    >
-      <slot name="left"></slot>
-    </div>
-    <slot>
-      <div class="d-flex justify-center pt-2 text-center pb-4">
-        <slot name="prepend">
-          <v-icon v-if="icon" large left>{{ icon }}</v-icon>
-        </slot>
-        <div class="d-flex flex-column">
-          <slot name="title">
-            <h1>
-              {{ title }}
-            </h1>
-          </slot>
-          <slot name="subtitle">
-            <h3 v-if="subtitle" class="grey--text text--darken-1">
-              {{ subtitle }}
-            </h3>
-          </slot>
-        </div>
-        <slot name="append"> </slot>
-      </div>
-    </slot>
-    <div
-      class="align-self-end align-self-sm-center text-no-wrap hidden-xs-only"
-    >
-      <slot name="right"></slot>
-    </div>
-  </v-card> -->
 </template>
 
 <script>
@@ -72,9 +39,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-/* .v-card.v-sheet.theme--light {
-  border-bottom: 5px solid red !important;
-} */
-</style>
