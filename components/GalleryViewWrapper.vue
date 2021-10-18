1f28a1215d7478a966aa13623a1a0373d2c1a712
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2021-10-18 10:15:32 +0300

    update gallery view layout

--- components/GalleryViewWrapper.vue
+++ components/GalleryViewWrapper.vue
@@ -1,89 +1,27 @@
 <template>
   <v-card v-if="items && items.length > 0" flat>
-    <pagination
-      v-if="count > 10"
-      :paginate-by="options.itemsPerPage"
-      :page="options.page"
-      :count="count"
-      @update:paginateBy="updatePaginateBy($event)"
-      @update:page="updatePage($event)"
+    <pagination-controls
+      :options="options"
+      :pagination="pagination"
+      :items-per-page-options="footerProps['items-per-page-options']"
+      :items-per-page-text="footerProps['items-per-page-text']"
+      :page-select-text="
+        $t('common.pageSelect', {
+          current: options.page,
+          count: pagination.pageCount,
+        })
+      "
+      :go-to-text="$t('common.goTo')"
+      :go-to-button-text="$t('common.goToBtn')"
+      select-page-id="header-select-btn"
+      @update:options="updateOptions"
     />
 
     <v-card flat>
       <div class="px-4">
-        <v-carousel
-          v-model="activeIndex"
-          style="max-width: 1000px"
-          :height="carouselHeight"
-          hide-delimiters
-          hide-delimiter-background
-          class="rounded mx-auto gallery__v-carousel"
-        >
-          <v-carousel-item
-            v-for="(item, index) in items"
-            :key="index"
-            nuxt
-            :to="localePath({ name: 'photo-id', params: { id: item.id } })"
-            :src="
-              $img(
-                `${item.uuid_filename}`,
-                { size: 'medium' },
-                { provider: 'geocollections' }
-              )
-            "
-          />
-        </v-carousel>
-
-        <div
-          class="
-            mt-2
-            mx-2
-            d-flex
-            justify-center
-            flex-column
-            justify-sm-space-between
-            flex-sm-row
-            mx-auto
-          "
-          style="max-width: 1000px"
-        >
-          <div class="text-center text-sm-left">
-            <div
-              v-if="items[activeIndex].agent || items[activeIndex].author_free"
-            >
-              <b>{{ $t('photo.author') }}: </b
-              >{{ items[activeIndex].agent || items[activeIndex].author_free }}
-            </div>
-            <div
-              v-if="
-                items[activeIndex].date_created ||
-                items[activeIndex].date_created_free
-              "
-            >
-              <b>{{ $t('photo.date') }}: </b>
-              <span v-if="items[activeIndex].date_created">{{
-                items[activeIndex].date_created.split('T')[0]
-              }}</span>
-              <span v-else>{{ items[activeIndex].date_created_free }}</span>
-            </div>
-          </div>
-
-          <div class="text-center">
-            <span v-for="(size, index) in imageSizes" :key="index">
-              <a
-                class="text-link"
-                @click="$openImage(items[activeIndex].uuid_filename, size)"
-              >
-                {{ $t(`common.${size}`) }}
-              </a>
-              <span v-if="index < imageSizes.length - 1">| </span>
-            </span>
-          </div>
-        </div>
-
         <v-card-text v-if="items && items.length > 1" class="px-0 pt-0">
           <div class="d-flex align-center" style="overflow-x: auto">
-            <div v-for="(item, index) in items" :key="index" class="my-2 mr-4">
+            <div v-for="(item, index) in items" :key="index" class="my-2 mx-2">
               <v-hover v-slot="{ hover }">
                 <v-img
                   :src="
@@ -127,6 +65,82 @@
               </v-hover>
             </div>
           </div>
+          <v-carousel
+            v-model="activeIndex"
+            style="max-width: 1000px"
+            :height="carouselHeight"
+            hide-delimiters
+            hide-delimiter-background
+            class="mt-2 rounded mx-auto gallery__v-carousel"
+          >
+            <v-carousel-item
+              v-for="(item, index) in items"
+              :key="index"
+              nuxt
+              :to="localePath({ name: 'photo-id', params: { id: item.id } })"
+              :src="
+                $img(
+                  `${item.uuid_filename}`,
+                  { size: 'medium' },
+                  { provider: 'geocollections' }
+                )
+              "
+            />
+          </v-carousel>
+
+          <div
+            class="
+              d-flex
+              justify-center
+              flex-column
+              justify-sm-space-between
+              flex-sm-row
+            "
+            style="max-width: 1000px"
+          >
+            <div class="text-center text-sm-left">
+              <div
+                v-if="
+                  items[activeIndex].agent || items[activeIndex].author_free
+                "
+              >
+                <b>{{ $t('photo.author') }}: </b
+                >{{
+                  items[activeIndex].agent || items[activeIndex].author_free
+                }}
+              </div>
+              <div
+                v-if="
+                  items[activeIndex].date_created ||
+                  items[activeIndex].date_created_free
+                "
+              >
+                <b>{{ $t('photo.date') }}: </b>
+                <span v-if="items[activeIndex].date_created">{{
+                  items[activeIndex].date_created.split('T')[0]
+                }}</span>
+                <span v-else>{{ items[activeIndex].date_created_free }}</span>
+              </div>
+            </div>
+
+            <div class="text-center">
+              <span v-for="(size, index) in imageSizes" :key="index">
+                <a
+                  class="text-link"
+                  @click="$openImage(items[activeIndex].uuid_filename, size)"
+                >
+                  {{ $t(`common.${size}`) }}
+                  <v-icon
+                    v-if="size === 'original'"
+                    small
+                    color="primary darken-2"
+                    >mdi-file-download-outline</v-icon
+                  >
+                </a>
+                <span v-if="index < imageSizes.length - 1">| </span>
+              </span>
+            </div>
+          </div>
         </v-card-text>
       </div>
     </v-card>
@@ -134,10 +148,11 @@
 </template>
 
 <script>
-import Pagination from '~/components/Pagination'
+import PaginationControls from '~/components/tables/controls/PaginationControls.vue'
+
 export default {
   name: 'GalleryViewWrapper',
-  components: { Pagination },
+  components: { PaginationControls },
   props: {
     items: {
       type: Array,
@@ -159,6 +174,11 @@ export default {
     return {
       activeIndex: 0,
       imageSizes: ['small', 'medium', 'large', 'original'],
+      footerProps: {
+        showFirstLastPage: true,
+        'items-per-page-options': [10, 25, 50, 100, 250, 500, 1000],
+        'items-per-page-text': this.$t('table.itemsPerPage'),
+      },
     }
   },
   computed: {
@@ -166,6 +186,9 @@ export default {
       if (this.$vuetify.breakpoint.xsOnly) return '35vh'
       else if (this.$vuetify.breakpoint.smOnly) return '40vh'
       else return '600px'
+    },
+    pagination() {
+      return { pageCount: Math.ceil(this.count / this.options.itemsPerPage) }
     },
   },
   watch: {
@@ -183,20 +206,6 @@ export default {
     window.removeEventListener('keyup', this.handleKeyup)
   },
   methods: {
-    updatePaginateBy(paginateBy) {
-      this.$emit('update', {
-        options: {
-          ...this.options,
-          itemsPerPage: paginateBy,
-          page: 1,
-        },
-      })
-    },
-
-    updatePage(page) {
-      this.$emit('update', { options: { ...this.options, page } })
-    },
-
     handleThumbnailClick(newIndex) {
       this.activeIndex = newIndex
     },
@@ -213,6 +222,9 @@ export default {
           else this.activeIndex += 1
         }
       }
+    },
+    updateOptions(event) {
+      this.$emit('update', { options: event })
     },
   },
 }
