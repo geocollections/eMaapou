659092bc5f0e1c89f9238abab03b7bbb755010fe
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2022-01-28 15:34:12 +0200

    update translation

--- lang/et.js
+++ lang/et.js
@@ -314,7 +314,7 @@ export default {
     thickness: 'Paksus',
     image: 'Pilt',
     specimen: 'Eksemplar',
-    locality: 'Loc.',
+    locality: 'Lokaliteet',
     rock: 'Kivim',
     collectionNr: 'Kollektsiooni nr.',
     type: 'Tüüp',
