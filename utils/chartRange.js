34728f97769fb6e8ff206037cd6bbcc2da76360b
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2022-10-05 10:50:37 +0300

    fix max/min deoth calculation

--- utils/chartRange.js
+++ utils/chartRange.js
@@ -1,8 +1,7 @@
 export default function chartRange(maxs, mins) {
   let minDepth = Math.min(...mins)
   let maxDepth = Math.max(...maxs)
-  if (maxDepth > 0 && minDepth > 0 && maxDepth > minDepth)
+  if (maxDepth > 0 && minDepth >= 0)
     [maxDepth, minDepth] = [-minDepth, -maxDepth]
-
   return [maxDepth, minDepth]
 }
