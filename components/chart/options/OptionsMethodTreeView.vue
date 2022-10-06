a4fb28f25fbed7fb152210225462bfdcdae5b7a1
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2022-10-06 11:34:37 +0300

    add filtering

--- components/chart/options/OptionsMethodTreeView.vue
+++ components/chart/options/OptionsMethodTreeView.vue
@@ -19,22 +19,39 @@
         {{ $t('common.parameters') }}
       </v-btn>
     </template>
-    <v-treeview
-      style="max-height: 500px"
-      dense
-      selectable
-      return-object
-      :items="methods"
-      :value="initialSelection"
-      @input="handleInput"
-    >
-      <template #label="{ item }">
-        <div>
-          {{ $translate({ et: item.name, en: item.name_en }) }}
-          <v-chip small>{{ item.count }}</v-chip>
+    <v-card flat style="min-width: 400px">
+      <v-card-title class="text-body-1 pb-0 px-2">
+        <div class="montserrat">
+          {{ $t('common.parameters') }}
         </div>
-      </template>
-    </v-treeview>
+      </v-card-title>
+      <v-card-text class="px-2">
+        <v-text-field
+          v-model="search"
+          :label="$t('common.filter')"
+          class="py-2"
+          dense
+          hide-details
+        />
+        <v-treeview
+          style="max-height: 500px"
+          dense
+          selectable
+          :search="search"
+          return-object
+          :items="methods"
+          :value="initialSelection"
+          @input="handleInput"
+        >
+          <template #label="{ item }">
+            <div>
+              {{ $translate({ et: item.name, en: item.name_en }) }}
+              <v-chip small>{{ item.count }}</v-chip>
+            </div>
+          </template>
+        </v-treeview>
+      </v-card-text>
+    </v-card>
   </v-menu>
 </template>
 
@@ -50,6 +67,11 @@ export default {
       type: Array,
       default: () => [],
     },
+  },
+  data() {
+    return {
+      search: '',
+    }
   },
   methods: {
     handleInput(event) {
