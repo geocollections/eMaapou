9675758b3e0e074bf3c8f95d0175ea20b32bc50c
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2022-10-13 10:01:14 +0300

    remove conditional switching

--- utils/chartRange.js
+++ utils/chartRange.js
@@ -1,7 +1,4 @@
 export default function chartRange(maxs, mins) {
-  let minDepth = Math.min(...mins)
-  let maxDepth = Math.max(...maxs)
-  if (maxDepth > 0 && minDepth >= 0)
-    [maxDepth, minDepth] = [-minDepth, -maxDepth]
+  const [maxDepth, minDepth] = [-Math.min(...mins), -Math.max(...maxs)]
   return [maxDepth, minDepth]
 }
