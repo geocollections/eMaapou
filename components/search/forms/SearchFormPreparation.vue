0f06a169abc746c42d837b544f3d0ee4e76742c2
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2022-10-28 16:50:08 +0300

    #843 Preparation search: support query params

--- components/search/forms/SearchFormPreparation.vue
+++ components/search/forms/SearchFormPreparation.vue
@@ -1,7 +1,7 @@
 <template>
   <v-form @submit.prevent="handleSearch">
     <input-search v-model="query" />
-    <search-actions class="mb-3" :count="count" @click="handleReset" />
+    <search-actions class="mb-3" @click="handleReset" />
     <search-fields-wrapper :active="hasActiveFilters('preparation')">
       <input-text v-model="number" :label="$t(filters.byIds.number.label)" />
       <input-text
@@ -27,16 +27,17 @@
       class="mt-2"
       :active="!isEmpty(institution)"
       :institution="institution"
-      @change:institution="institution = $event"
+      @change:institution="handleInstitutionsUpdate"
     />
   </v-form>
 </template>
 
-<script>
-import { mapState, mapActions, mapGetters } from 'vuex'
+<script lang="ts">
+import { mapState, mapGetters } from 'vuex'
 import { mapFields } from 'vuex-map-fields'
 import isEmpty from 'lodash/isEmpty'
 
+import Vue from 'vue'
 import SearchFieldsWrapper from '../SearchFieldsWrapper.vue'
 import SearchActions from '../SearchActions.vue'
 import SearchInstitutionFilter from '~/components/search/SearchInstitutionFilter.vue'
@@ -45,8 +46,7 @@ import InputRange from '~/components/input/InputRange.vue'
 import SearchMap from '~/components/search/SearchMap.vue'
 import InputSearch from '~/components/input/InputSearch.vue'
 import InputAutocompleteStratigraphy from '~/components/input/InputAutocompleteStratigraphy.vue'
-
-export default {
+export default Vue.extend({
   name: 'SearchFormPreparation',
   components: {
     SearchInstitutionFilter,
@@ -59,7 +59,7 @@ export default {
     InputSearch,
   },
   computed: {
-    ...mapState('search/preparation', ['filters', 'count', 'items']),
+    ...mapState('search/preparation', ['filters', 'items']),
     ...mapFields('search/preparation', {
       number: 'filters.byIds.number.value',
       depth: 'filters.byIds.depth.value',
@@ -75,18 +75,19 @@ export default {
   },
   methods: {
     isEmpty,
-    ...mapActions('search', ['resetFilters']),
-    ...mapActions('search/preparation', ['searchPreparations']),
-    async handleReset() {
-      await this.resetFilters('preparation')
-      this.searchPreparations()
+    handleReset() {
+      this.$emit('reset')
     },
     handleSearch() {
-      this.searchPreparations()
+      this.$emit('update')
     },
-    handleMapUpdate(tableState) {
-      this.searchPreparations(tableState?.options)
+    handleMapUpdate() {
+      this.$emit('update')
+    },
+    handleInstitutionsUpdate(newInstitutions: any[]) {
+      this.institution = newInstitutions
+      this.$emit('update')
     },
   },
-}
+})
 </script>
--- pages/preparation/index.vue
+++ pages/preparation/index.vue
@@ -8,7 +8,10 @@
     </template>
 
     <template #form>
-      <search-form-preparation />
+      <search-form-preparation
+        @update="handleFormUpdate"
+        @reset="handleFormReset"
+      />
     </template>
 
     <template #result>
@@ -25,23 +28,28 @@
           stateful-headers
           dynamic-headers
           :is-loading="$fetchState.pending"
-          @update="handleUpdate"
+          @update="handleDataTableUpdate"
         />
       </v-card>
     </template>
   </search>
 </template>
 
-<script>
+<script lang="ts">
 import { mdiEyedropper } from '@mdi/js'
-import { mapState, mapActions } from 'vuex'
 import { mapFields } from 'vuex-map-fields'
+import Vue from 'vue'
+import { MetaInfo } from 'vue-meta'
+import isEqual from 'lodash/isEqual'
 import DataTablePreparation from '~/components/data-table/DataTablePreparation.vue'
 import SearchFormPreparation from '~/components/search/forms/SearchFormPreparation.vue'
 import Search from '~/templates/Search.vue'
 import BaseHeader from '~/components/base/BaseHeader.vue'
-
-export default {
+import getQueryParams from '~/utils/getQueryParams'
+import parseQueryParams from '~/utils/parseQueryParams'
+import { HEADERS_PREPARATION } from '~/constants'
+const qParamKey = 'preparationQ'
+export default Vue.extend({
   name: 'PreparationSearch',
   components: {
     Search,
@@ -50,15 +58,28 @@ export default {
     BaseHeader,
   },
   async fetch() {
-    await this.searchPreparations(this.options)
+    const response = await this.$services.sarvSolr.getResourceList(
+      'preparation',
+      {
+        options: this.options,
+        search: this.query,
+        fields: this.$getAPIFieldValues(HEADERS_PREPARATION),
+        searchFilters: {
+          ...this.$accessor.search.preparation.filters.byIds,
+          ...this.$accessor.search.globalFilters.byIds,
+        },
+      }
+    )
+    this.items = response.items
+    this.count = response.count
   },
-  head() {
+  head(): MetaInfo {
     return {
-      title: this.$t('preparation.pageTitle'),
+      title: this.$t('preparation.pageTitle') as string,
       meta: [
         {
           property: 'og:title',
-          content: this.$t('preparation.pageTitle'),
+          content: this.$t('preparation.pageTitle') as string,
           hid: 'og:title',
         },
         {
@@ -70,18 +91,104 @@ export default {
     }
   },
   computed: {
-    ...mapState('search/preparation', ['items', 'count']),
-    ...mapFields('search/preparation', ['options']),
-    icons() {
+    ...mapFields('search/preparation', {
+      query: 'query',
+      items: 'items',
+      count: 'count',
+      options: 'options',
+      number: 'filters.byIds.number.value',
+      depth: 'filters.byIds.depth.value',
+      locality: 'filters.byIds.locality.value',
+      hierarchy: 'filters.byIds.hierarchy.value',
+    }),
+    icons(): any {
       return { mdiEyedropper }
     },
+    queryParams(): { [K: string]: any } {
+      return getQueryParams({
+        q: { key: qParamKey, value: this.$accessor.search.preparation.query },
+        filters: this.$accessor.search.preparation.filters.byIds,
+        globalFilters: this.$accessor.search.globalFilters.byIds,
+        tableOptions: this.options,
+      })
+    },
+  },
+  watch: {
+    '$route.query': {
+      async handler() {
+        await this.$accessor.search.resetFilters('preparation')
+        this.setStateFromQueryParams()
+        this.$fetch()
+      },
+    },
+  },
+  created() {
+    // Add global filters and table options to query params, if they are missing
+    const query = getQueryParams({
+      globalFilters: this.$accessor.search.globalFilters.byIds,
+      // @ts-ignore
+      tableOptions: this.options,
+    })
+    if (!isEqual({ ...query, ...this.$route.query }, this.$route.query))
+      this.$router.replace({ query: { ...query, ...this.$route.query } })
+
+    this.setStateFromQueryParams()
   },
   methods: {
-    ...mapActions('search/preparation', ['searchPreparations']),
-    handleUpdate(tableState) {
+    setStateFromQueryParams() {
+      const parsedValues = parseQueryParams({
+        route: this.$route,
+        filters: this.$accessor.search.preparation.filters.byIds,
+        globalFilters: this.$accessor.search.globalFilters.byIds,
+        qKey: qParamKey,
+      })
+      this.query = parsedValues.query
+      if (parsedValues.filters) {
+        Object.keys(parsedValues.filters).forEach((key) => {
+          // @ts-ignore
+          this[key] = parsedValues.filters?.[key]
+        })
+      }
+      if (parsedValues.globalFilters) {
+        Object.keys(parsedValues.globalFilters).forEach((key) => {
+          // @ts-ignore
+          this[key] = parsedValues.globalFilters?.[key]
+        })
+      }
+      this.options = {
+        ...this.options,
+        ...parsedValues.options,
+      }
+    },
+    async handleFormReset() {
+      this.options.page = 1
+
+      // NOTE: https://github.com/nuxt/nuxt.js/issues/6951#issuecomment-904655674
+      await new Promise((resolve, reject) =>
+        this.$router.push(
+          { path: this.$route.path, query: {} },
+          resolve,
+          reject
+        )
+      )
+      await this.$accessor.search.resetFilters('preparation')
+      this.$fetch()
+    },
+    async handleFormUpdate() {
+      this.options.page = 1
+
+      await new Promise((resolve, reject) =>
+        this.$router.push({ query: this.queryParams }, resolve, reject)
+      )
+      this.$fetch()
+    },
+    async handleDataTableUpdate(tableState: any) {
       this.options = tableState.options
+      await new Promise((resolve, reject) =>
+        this.$router.push({ query: this.queryParams }, resolve, reject)
+      )
       this.$fetch()
     },
   },
-}
+})
 </script>
