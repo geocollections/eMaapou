1e77e54a55df7fe99763555f57ce61b8c053ff55
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2022-11-15 12:22:03 +0200

    Analysis detail: add analysis result chart

--- components/chart/ChartAnalysisResults.vue
+++ components/chart/ChartAnalysisResults.vue
@@ -1,10 +1,161 @@
 <template>
-  <div></div>
+  <div>
+    <client-only>
+      <v-chart
+        id="chart-analysis-results"
+        ref="chart"
+        :style="`height: ${totalHeight}px; width: 100%`"
+        :init-options="initOptions"
+        :option="option"
+      />
+    </client-only>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import {
+  computed,
+  defineComponent,
+  nextTick,
+  onMounted,
+  PropType,
+} from '@nuxtjs/composition-api'
+import { GridComponentOption } from 'echarts/components'
+import { ComposeOption } from 'echarts/core'
+import {
+  XAXisComponentOption,
+  YAXisComponentOption,
+} from 'echarts/types/dist/echarts'
+import {
+  LegendOption,
+  BarSeriesOption,
+  TooltipOption,
+} from 'echarts/types/dist/shared'
+
+type ECOption = ComposeOption<
+  | TooltipOption
+  | LegendOption
+  | BarSeriesOption
+  | XAXisComponentOption
+  | YAXisComponentOption
+  | GridComponentOption
+>
+
 export default defineComponent({
   name: 'ChartAnalysisResults',
+  props: {
+    data: {
+      type: Object as PropType<{
+        [K: number | string]: BarSeriesOption['data']
+      }>,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const initOptions = computed(() => {
+      return { renderer: 'canvas' }
+    })
+    const unitGroup = 1
+
+    const createSubCharts = (data: typeof props['data']) => {
+      let topPosition = 20
+      let totalHeight = 60
+      return {
+        subCharts: Object.values(data).reduce(
+          (
+            prev,
+            group,
+            i
+          ): {
+            grid: GridComponentOption[]
+            xAxis: XAXisComponentOption[]
+            yAxis: YAXisComponentOption[]
+            series: BarSeriesOption[]
+          } => {
+            const height = 20 * (group?.length ?? 0)
+            const result = {
+              ...prev,
+              grid: [
+                ...prev.grid,
+                {
+                  id: `analysis-result-grid-${i}`,
+                  height,
+                  top: topPosition + 40 * i,
+                  left: '15%',
+                },
+              ] as GridComponentOption[],
+              xAxis: [
+                ...prev.xAxis,
+                { gridIndex: i, position: 'top' },
+              ] as XAXisComponentOption[],
+              yAxis: [
+                ...prev.yAxis,
+                {
+                  gridIndex: i,
+                  type: 'category',
+                  // @ts-ignore
+                  data: group?.map((val) => val.name),
+                  axisPointer: {
+                    show: true,
+                    type: 'shadow',
+                  },
+                },
+              ] as YAXisComponentOption[],
+              series: [
+                ...prev.series,
+                {
+                  type: 'bar',
+                  data: group,
+                  xAxisIndex: i,
+                  yAxisIndex: i,
+                },
+              ] as BarSeriesOption[],
+            }
+            topPosition += height
+            totalHeight += height + 40 * i
+            return result
+          },
+          {
+            grid: [] as GridComponentOption[],
+            xAxis: [] as XAXisComponentOption[],
+            yAxis: [] as YAXisComponentOption[],
+            series: [] as BarSeriesOption[],
+          }
+        ),
+        chartOptions: { totalHeight },
+      }
+    }
+    const { subCharts, chartOptions } = createSubCharts(props.data)
+
+    const option = computed((): ECOption => {
+      return {
+        tooltip: {
+          trigger: 'item',
+        },
+        legend: {
+          type: 'scroll',
+          orient: 'vertical',
+          right: 10,
+          top: 20,
+          bottom: 20,
+          // @ts-ignore
+          data: Object.values(props.data)[unitGroup]?.map((val) => val.name),
+        },
+        grid: subCharts.grid,
+        xAxis: subCharts.xAxis,
+        yAxis: subCharts.yAxis,
+        series: subCharts.series,
+      }
+    })
+    onMounted(() => {
+      nextTick(() => {
+        window.addEventListener('resize', () => {
+          // @ts-ignore
+          context.refs.chart.resize()
+        })
+      })
+    })
+    return { initOptions, option, totalHeight: chartOptions.totalHeight }
+  },
 })
 </script>
--- pages/analysis/_id.vue
+++ pages/analysis/_id.vue
@@ -4,153 +4,159 @@
       <header-detail :ids="ids" :title="pageTitle" />
     </template>
 
-    <template #default>
-      <v-card>
-        <v-row no-gutters justify="center">
-          <v-col>
-            <v-card-title class="subsection-title">{{
-              $t('common.general')
-            }}</v-card-title>
-            <v-card-text>
-              <base-table>
-                <table-row
-                  v-if="analysisMethod"
-                  :title="$t('analysis.method')"
-                  :value="
-                    $translate({
-                      et: analysisMethod.analysis_method,
-                      en: analysisMethod.method_en,
-                    })
-                  "
-                />
-                <table-row
-                  v-if="agent"
-                  :title="$t('analysis.analysedBy')"
-                  :value="agent.agent"
-                />
+    <template #column-left>
+      <v-row no-gutters justify="center">
+        <v-col>
+          <v-card-title class="subsection-title">{{
+            $t('common.general')
+          }}</v-card-title>
+          <v-card-text>
+            <base-table>
+              <table-row
+                v-if="analysisMethod"
+                :title="$t('analysis.method')"
+                :value="
+                  $translate({
+                    et: analysisMethod.analysis_method,
+                    en: analysisMethod.method_en,
+                  })
+                "
+              />
+              <table-row
+                v-if="agent"
+                :title="$t('analysis.analysedBy')"
+                :value="agent.agent"
+              />
 
-                <table-row-link
-                  v-if="sample"
-                  :title="$t('analysis.sampleNumber')"
-                  :value="sample.number"
-                  nuxt
-                  :href="
-                    localePath({
-                      name: 'sample-id',
-                      params: { id: analysis.sample.id },
-                    })
-                  "
-                />
+              <table-row-link
+                v-if="sample"
+                :title="$t('analysis.sampleNumber')"
+                :value="sample.number"
+                nuxt
+                :href="
+                  localePath({
+                    name: 'sample-id',
+                    params: { id: analysis.sample.id },
+                  })
+                "
+              />
 
-                <table-row
-                  :title="$t('analysis.remarks')"
-                  :value="analysis.remarks"
-                />
-                <table-row-link
-                  v-if="reference"
-                  :title="$t('analysis.reference')"
-                  :value="reference.reference"
-                  @link-click="$openGeology('reference', reference.id)"
-                />
-                <table-row
-                  v-if="dataset"
-                  :title="$t('analysis.dataset')"
-                  :value="
-                    $translate({
-                      et: dataset.name,
-                      en: dataset.name_en,
-                    })
-                  "
-                />
-                <table-row-link
-                  v-if="sample && sample.locality"
-                  :title="$t('analysis.locality')"
-                  :value="
-                    $translate({
-                      et: sample.locality.locality,
-                      en: sample.locality.locality_en,
-                    })
-                  "
-                  nuxt
-                  :href="
-                    localePath({
-                      name: 'locality-id',
-                      params: { id: sample.locality.id },
-                    })
-                  "
-                />
-                <table-row
-                  v-if="sample"
-                  :title="$t('analysis.depth')"
-                  :value="sample.depth"
-                />
-                <table-row
-                  v-if="sample"
-                  :title="$t('analysis.depthInterval')"
-                  :value="sample.depth_interval"
-                />
-                <table-row-link
-                  v-if="sample && sample.stratigraphy"
-                  :title="$t('analysis.stratigraphy')"
-                  :value="
-                    $translate({
-                      et: sample.stratigraphy.stratigraphy,
-                      en: sample.stratigraphy.stratigraphy_en,
-                    })
-                  "
-                  nuxt
-                  :href="
-                    localePath({
-                      name: 'stratigraphy-id',
-                      params: { id: sample.stratigraphy.id },
-                    })
-                  "
-                />
-                <table-row-link
-                  v-if="sample && sample.lithostratigraphy"
-                  :title="$t('analysis.lithostratigraphy')"
-                  :value="
-                    $translate({
-                      et: sample.lithostratigraphy.stratigraphy,
-                      en: sample.lithostratigraphy.stratigraphy_en,
-                    })
-                  "
-                  nuxt
-                  :href="
-                    localePath({
-                      name: 'stratigraphy-id',
-                      params: { id: sample.lithostratigraphy.id },
-                    })
-                  "
-                />
-                <table-row-link
-                  v-if="database"
-                  :title="$t('analysis.institution')"
-                  :value="
-                    $translate({
-                      et: database.name,
-                      en: database.name_en,
-                    })
-                  "
-                  :href="database.url"
-                  target="DatabaseWindow"
-                />
+              <table-row
+                :title="$t('analysis.remarks')"
+                :value="analysis.remarks"
+              />
+              <table-row-link
+                v-if="reference"
+                :title="$t('analysis.reference')"
+                :value="reference.reference"
+                @link-click="$openGeology('reference', reference.id)"
+              />
+              <table-row
+                v-if="dataset"
+                :title="$t('analysis.dataset')"
+                :value="
+                  $translate({
+                    et: dataset.name,
+                    en: dataset.name_en,
+                  })
+                "
+              />
+              <table-row-link
+                v-if="sample && sample.locality"
+                :title="$t('analysis.locality')"
+                :value="
+                  $translate({
+                    et: sample.locality.locality,
+                    en: sample.locality.locality_en,
+                  })
+                "
+                nuxt
+                :href="
+                  localePath({
+                    name: 'locality-id',
+                    params: { id: sample.locality.id },
+                  })
+                "
+              />
+              <table-row
+                v-if="sample"
+                :title="$t('analysis.depth')"
+                :value="sample.depth"
+              />
+              <table-row
+                v-if="sample"
+                :title="$t('analysis.depthInterval')"
+                :value="sample.depth_interval"
+              />
+              <table-row-link
+                v-if="sample && sample.stratigraphy"
+                :title="$t('analysis.stratigraphy')"
+                :value="
+                  $translate({
+                    et: sample.stratigraphy.stratigraphy,
+                    en: sample.stratigraphy.stratigraphy_en,
+                  })
+                "
+                nuxt
+                :href="
+                  localePath({
+                    name: 'stratigraphy-id',
+                    params: { id: sample.stratigraphy.id },
+                  })
+                "
+              />
+              <table-row-link
+                v-if="sample && sample.lithostratigraphy"
+                :title="$t('analysis.lithostratigraphy')"
+                :value="
+                  $translate({
+                    et: sample.lithostratigraphy.stratigraphy,
+                    en: sample.lithostratigraphy.stratigraphy_en,
+                  })
+                "
+                nuxt
+                :href="
+                  localePath({
+                    name: 'stratigraphy-id',
+                    params: { id: sample.lithostratigraphy.id },
+                  })
+                "
+              />
+              <table-row-link
+                v-if="database"
+                :title="$t('analysis.institution')"
+                :value="
+                  $translate({
+                    et: database.name,
+                    en: database.name_en,
+                  })
+                "
+                :href="database.url"
+                target="DatabaseWindow"
+              />
 
-                <table-row
-                  v-if="analysis.date_added"
-                  :title="$t('analysis.dateAdded')"
-                  :value="$formatDate(analysis.date_added)"
-                />
-                <table-row
-                  v-if="analysis.date_changed"
-                  :title="$t('analysis.dateChanged')"
-                  :value="$formatDate(analysis.date_changed)"
-                />
-              </base-table>
-            </v-card-text>
-          </v-col>
-        </v-row>
-      </v-card>
+              <table-row
+                v-if="analysis.date_added"
+                :title="$t('analysis.dateAdded')"
+                :value="$formatDate(analysis.date_added)"
+              />
+              <table-row
+                v-if="analysis.date_changed"
+                :title="$t('analysis.dateChanged')"
+                :value="$formatDate(analysis.date_changed)"
+              />
+            </base-table>
+          </v-card-text>
+        </v-col>
+      </v-row>
+    </template>
+    <template v-if="resultChartData" #column-right>
+      <v-row no-gutters justify="center">
+        <v-col>
+          <v-card-title class="subsection-title">Results </v-card-title>
+          <chart-analysis-results :data="resultChartData" />
+        </v-col>
+      </v-row>
     </template>
     <template #bottom>
       <v-card v-if="tabs.length > 0" class="mt-4 mb-4">
@@ -175,6 +181,8 @@ import {
   useRoute,
 } from '@nuxtjs/composition-api'
 import { Location } from 'vue-router'
+import sumBy from 'lodash/sumBy'
+import sortBy from 'lodash/sortBy'
 import HeaderDetail from '~/components/HeaderDetail.vue'
 import TableRow from '~/components/table/TableRow.vue'
 import TableRowLink from '~/components/table/TableRowLink.vue'
@@ -183,8 +191,17 @@ import Detail from '~/templates/Detail.vue'
 import BaseTable from '~/components/base/BaseTable.vue'
 import { Tab, TABS_ANALYSIS } from '~/constants'
 import { useSlugRoute } from '~/composables/useSlugRoute'
+import ChartAnalysisResults from '~/components/chart/ChartAnalysisResults.vue'
 export default defineComponent({
-  components: { HeaderDetail, TableRow, TableRowLink, Tabs, Detail, BaseTable },
+  components: {
+    HeaderDetail,
+    TableRow,
+    TableRowLink,
+    Tabs,
+    Detail,
+    BaseTable,
+    ChartAnalysisResults,
+  },
   setup() {
     const { $services, $hydrateTab, $translate, i18n } = useContext()
     const route = useRoute()
@@ -193,6 +210,7 @@ export default defineComponent({
       ids: {} as any,
       validRoute: {} as Location,
       tabs: [] as Tab[],
+      resultChartData: {} as { [K: number]: any[] },
     })
 
     const database = computed(() => state.analysis?.database)
@@ -229,13 +247,53 @@ export default defineComponent({
             })
         )
       )
-      const [analysisResponse, hydratedTabs] = await Promise.all([
-        analysisPromise,
-        hydratedTabsPromise,
-      ])
+      const analysisResultPromise = $services.sarvSolr.getResourceList(
+        'analysis_results',
+        {
+          defaultParams: {
+            fq: `analysis_id:${route.value.params.id}`,
+            group: true,
+            'group.field': 'unit_id',
+            'group.sort': 'value desc',
+            'group.limit': 100,
+          },
+        }
+      )
+      const [analysisResponse, analysisResultResponse, hydratedTabs] =
+        await Promise.all([
+          analysisPromise,
+          analysisResultPromise,
+          hydratedTabsPromise,
+        ])
       state.tabs = hydratedTabs.filter((tab) => tab.count > 0)
       state.ids = analysisResponse?.ids
       state.analysis = analysisResponse
+
+      state.resultChartData = analysisResultResponse.grouped.unit_id.groups
+        .map((group: any) => [group.groupValue, group.doclist.docs])
+        .reduce((prev: any, [unitKey, unitResults]: any) => {
+          const results = unitResults
+            .filter((result: any) => result.value && result.value >= 0)
+            .map((result: any) => {
+              return {
+                name: result.parameter,
+                value: result.value,
+              }
+            })
+
+          const valueSum = sumBy(results, 'value')
+          if (unitKey === '6') {
+            results.push({ name: 'Other', value: 100 - valueSum })
+          }
+          // if (unitKey === '7') {
+          //   results.push({ name: 'Other', value: 1000000 - valueSum })
+          // }
+
+          return {
+            ...prev,
+            [unitKey]: sortBy(results, 'value'),
+          }
+        }, {})
     })
     const slugText = computed(
       () =>
