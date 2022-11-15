a1228ac1a73f68fd8f3ba25ea28deb60653c849a
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2022-11-15 13:09:54 +0200

    check if `to` and `from` `id` param are different before adding history

--- plugins/afterEach.ts
+++ plugins/afterEach.ts
@@ -19,11 +19,12 @@ const INCLUDED_PAGES = [
   'stratigraphy-id',
 ]
 export default defineNuxtPlugin(({ app, $accessor }, _) => {
-  app.router?.afterEach((_, from) => {
+  app.router?.afterEach((to, from) => {
     const title = document.title
     const parsedTitle = title.substring(0, title.indexOf('|')).trim()
     if (app.nuxt.err) return
     if (parsedTitle === 'undefined') return
+    if (to.params.id === from.params.id) return
     if (!INCLUDED_PAGES.some((page) => from.name?.includes(page))) return
 
     const historyObject = {
