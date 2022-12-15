67d105c70217669e2a4c96e3813144b2762e4d68
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2022-12-15 10:43:06 +0200

    history viewer: ignore error pages

--- plugins/afterEach.ts
+++ plugins/afterEach.ts
@@ -18,13 +18,14 @@ const INCLUDED_PAGES = [
   'specimen-id',
   'stratigraphy-id',
 ]
-export default defineNuxtPlugin(({ app, $accessor }, _) => {
+export default defineNuxtPlugin(({ app, $accessor, i18n }, _) => {
   app.router?.afterEach((to, from) => {
     const title = document.title
     const parsedTitle = title.substring(0, title.indexOf('|')).trim()
     if (app.nuxt.err) return
     if (parsedTitle === 'undefined') return
     if (to.params.id === from.params.id) return
+    if (parsedTitle === i18n.t('error.occurred')) return
     if (!INCLUDED_PAGES.some((page) => from.name?.includes(page))) return
 
     const historyObject = {
