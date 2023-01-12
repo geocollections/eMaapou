6aad0ef01e6664e0db2b06ee635b00dcfae2d997
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-01-12 16:51:03 +0200

    fix ts error

--- store/search/analyticalData/getters.ts
+++ store/search/analyticalData/getters.ts
@@ -13,12 +13,12 @@ export default getterTree(state, {
       // also filter out the parameter field of current filter
       // so that it can be seen on in the list
 
-      let usedParameters = state.parameterFilters.allIds.map(
+      const usedParameters = state.parameterFilters.allIds.map(
         (id) => state.parameterFilters.byId[id].fields[0]
       )
 
       if (filter) {
-        usedParameters = usedParameters.filter((v) => v !== filter.fields[0])
+        // usedParameters = usedParameters.filter((v) => v !== filter.fields[0])
 
         return [
           ...Object.values(state.parameters).filter((parameter) => {
