4f44b863f8a321cc78d332237be2b2722ef01efa
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-02-08 09:09:55 +0200

    update taxon filters

--- components/search/forms/SearchFormTaxon.vue
+++ components/search/forms/SearchFormTaxon.vue
@@ -8,8 +8,20 @@
           v-model="species"
           :title="$t('filters.species').toString()"
         />
-        <filter-stratigraphy v-model="stratigraphyHierarchy" />
-        <filter-taxon v-model="taxonHierarchy" />
+        <!-- <filter-input-autocomplete-new -->
+        <!--   v-model="stratigraphyHierarchy" -->
+        <!--   :title="$t('filters.stratigraphyHierarchy').toString()" -->
+        <!--   :query-field=" -->
+        <!--     $i18n.locale === 'et' ? 'stratigraphy' : 'startigraphy_en' -->
+        <!--   " -->
+        <!--   :query-function="querySuggestionsStratigraphy" -->
+        <!-- /> -->
+        <filter-input-autocomplete-new
+          v-model="taxonHierarchy"
+          :title="$t('filters.taxonHierarchy').toString()"
+          query-field="taxon"
+          :query-function="querySuggestionsTaxon"
+        />
         <filter-map
           v-model="map"
           locality-overlay
@@ -30,6 +42,7 @@ import {
   computed,
   defineComponent,
   ref,
+  toRef,
   useContext,
   useFetch,
   useRoute,
@@ -37,39 +50,34 @@ import {
 } from '@nuxtjs/composition-api'
 import SearchActions from '../SearchActions.vue'
 import InputSearch from '~/components/input/InputSearch.vue'
-import FilterStratigraphy from '~/components/filter/FilterStratigraphy.vue'
-import FilterTaxon from '~/components/filter/FilterTaxon.vue'
 import FilterMap from '~/components/filter/FilterMap.vue'
 import FilterInputText from '~/components/filter/input/FilterInputText.vue'
-import {
-  useHydrateFilterStratigraphy,
-  useHydrateFilterTaxon,
-} from '~/composables/useHydrateFilter'
+import FilterInputAutocompleteNew from '~/components/filter/input/FilterInputAutocompleteNew.vue'
+import { useHydrate, useHydrateFilterNew } from '~/composables/useHydrateFilter'
 import { useFilter } from '~/composables/useFilter'
+import { useQuerySuggestions } from '~/composables/useQuerySuggestions'
 export default defineComponent({
   name: 'SearchFormTaxon',
   components: {
     SearchActions,
     InputSearch,
-    FilterStratigraphy,
-    FilterTaxon,
     FilterMap,
     FilterInputText,
+    FilterInputAutocompleteNew,
   },
   setup(_props, { emit }) {
     const { $accessor } = useContext()
     const route = useRoute()
     const emitUpdate = ref(true)
-    const hydrateFilters = ref(true)
     const handleReset = () => {
       emit('reset')
     }
     const handleUpdate = () => {
       if (!emitUpdate.value) return
-      hydrateFilters.value = false
       emit('update')
     }
 
+    const filters = computed(() => $accessor.search.taxon.filters)
     const query = computed({
       get: () => $accessor.search.taxon.query,
       set: (val) => {
@@ -86,33 +94,130 @@ export default defineComponent({
     const taxonHierarchy = useFilter('taxon', 'taxonHierarchy', handleUpdate)
     const map = useFilter('taxon', 'map', handleUpdate)
 
-    const hydrateFilterStratigraphy = useHydrateFilterStratigraphy()
-    const hydrateFilterTaxon = useHydrateFilterTaxon()
-
     watch(
       () => route.value.query,
-      () => {
-        if (!hydrateFilters.value) {
-          hydrateFilters.value = true
-          return
-        }
-        fetch()
-      }
+      () => fetch()
     )
 
     const { fetch } = useFetch(async () => {
       emitUpdate.value = false
       await Promise.all([
-        hydrateFilterTaxon(taxonHierarchy, 'taxonHierarchy'),
-        hydrateFilterStratigraphy(
-          stratigraphyHierarchy,
-          'stratigraphyHierarchy'
+        hydrateFilter(
+          taxonHierarchy,
+          toRef(filters.value, 'taxonHierarchy'),
+          'taxonHierarchy',
+          hydrateTaxonHierarchy
         ),
+        // hydrateFilter(
+        //   stratigraphyHierarchy,
+        //   toRef(filters.value, 'stratigraphyHierarchy'),
+        //   'stratigraphyHierarchy',
+        //   hydrateStratigraphyHierarchy
+        // ),
       ])
       emitUpdate.value = true
     })
+    const hydrateFilter = useHydrateFilterNew()
+    const hydrate = useHydrate()
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const hydrateStratigraphyHierarchy = hydrate(
+      filters.value.stratigraphyHierarchy,
+      query,
+      {
+        itemResource: 'stratigraphy',
+        itemFields: [
+          'id',
+          'stratigraphy',
+          'stratigraphy_en',
+          'hierarchy_string',
+        ],
+        itemSearchField: 'hierarchy_string',
+        countResource: 'taxon',
+        countResourceRelatedIdKey: 'stratigraphy_hierarchy',
+        countHierarchical: true,
+        tagFilterKey: 'stratigraphyHierarchy',
+        filters,
+      },
+      (items, counts) =>
+        items.map((item: any) => ({
+          id: item.hierarchy_string,
+          text: item.stratigraphy,
+          text_en: item.stratigraphy_en,
+          hierarchy_string: item.hierarchy_string,
+          count: counts[item.hierarchy_string],
+        }))
+    )
+    const hydrateTaxonHierarchy = hydrate(
+      filters.value.taxonHierarchy,
+      query,
+      {
+        itemResource: 'taxon',
+        itemFields: ['id', 'taxon', 'hierarchy_string'],
+        itemSearchField: 'hierarchy_string',
+        countResource: 'taxon',
+        countResourceRelatedIdKey: 'hierarchy_string',
+        countHierarchical: true,
+        tagFilterKey: 'taxonHierarchy',
+        filters,
+      },
+      (items, counts) =>
+        items.map((item: any) => ({
+          id: item.hierarchy_string,
+          text: item.taxon,
+          text_en: item.taxon,
+          hierarchy_string: item.hierarchy_string,
+          count: counts[item.hierarchy_string],
+        }))
+    )
+    const querySuggestions = useQuerySuggestions()
+    const querySuggestionsStratigraphy = querySuggestions(
+      query,
+      {
+        resource: 'taxon',
+        pivot: ['stratigraphy_hierarchy', 'stratigraphy', 'stratigraphy_en'],
+        pivotOffsetField: 'stratigraphy_hierarchy',
+        countResourceRelatedKey: 'stratigraphy_hierarchy',
+        countHierarchical: true,
+        excludeFilterKey: 'stratigraphyHierarchy',
+        filters,
+      },
+      (items, counts) =>
+        items.map((item: any) => ({
+          id: item.value as number,
+          count: counts[item.value] as number,
+          text: item.pivot?.[0].value ?? item.value,
+          text_en:
+            item.pivot?.[0].pivot?.[0].value ??
+            item.pivot?.[0].value ??
+            item.value,
+          hierarchy_string: item.value,
+        }))
+    )
+    const querySuggestionsTaxon = querySuggestions(
+      query,
+      {
+        resource: 'taxon',
+        pivot: ['hierarchy_string', 'taxon'],
+        pivotOffsetField: 'hierarchy_string',
+        countResourceRelatedKey: 'hierarchy_string',
+        countHierarchical: true,
+        excludeFilterKey: 'taxonHierarchy',
+        filters,
+      },
+      (items, counts) =>
+        items.map((item: any) => ({
+          id: item.value as number,
+          count: counts[item.value] as number,
+          text: item.pivot?.[0].value ?? item.value,
+          text_en: item.pivot?.[0].value ?? item.value,
+          hierarchy_string: item.value,
+        }))
+    )
     return {
       query,
+      querySuggestionsStratigraphy,
+      querySuggestionsTaxon,
       stratigraphyHierarchy,
       taxonHierarchy,
       map,
