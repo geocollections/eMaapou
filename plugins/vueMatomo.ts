a00a9839d20476a30ef4169aff56fcca83953815
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-02-09 10:57:51 +0200

    enable heartbeat timer

--- plugins/vueMatomo.ts
+++ plugins/vueMatomo.ts
@@ -5,17 +5,16 @@ import VueMatomo from 'vue-matomo'
 // For configurations visit:  https://github.com/AmazingDreams/vue-matomo
 export default defineNuxtPlugin(({ app }) => {
   if (process.env.NODE_ENV === 'production') {
-
     Vue.use(VueMatomo, {
       router: app.router,
       host: 'https://matomo.geoloogia.info/',
-        siteId: 1,
+      siteId: 1,
       requireCookieConsent: true,
       requireConsent: false,
       debug: process.env.NODE_ENV !== 'production',
       enableLinkTracking: process.env.NODE_ENV === 'production',
-      trackInitialView: process.env.NODE_ENV === 'production'
+      trackInitialView: process.env.NODE_ENV === 'production',
+      enableHeartBeatTimer: true,
     })
   }
-
 })
