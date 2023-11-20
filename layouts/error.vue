d5e62b651fb39abd56df5b3074097d4aba9c3d18
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2023-11-20 10:48:59 +0200

    fix: error page padding
    Closes #1003

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <detail-error :error="error" />
-  </div>
+  <v-main>
+    <v-container :fluid="$vuetify.breakpoint.lgAndDown">
+      <v-row no-gutters>
+        <v-col>
+          <div class="pt-15">
+            <detail-error :error="error" />
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
