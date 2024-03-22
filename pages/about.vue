e53977f2df6c2cd836e98c21147d736ff0a2dc6a
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-03-22 13:29:20 +0200

    feat: add about and institutions pages

---
+++ pages/about.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const { data: page } = await useGeoloogiaApiFetch("/web_pages/78/");
+
+const { t } = useI18n();
+const { $translate } = useNuxtApp();
+
+useHead({
+  title: t("about.pageTitle"),
+});
+</script>
+
+<template>
+  <VMain>
+    <div class="pt-5 mx-auto" style="max-width: 800px">
+      <BaseHeader
+        :title="$t('about.title')"
+        class="mb-2 mx-3"
+        style="border-color: var(--v-header-darken1)"
+      />
+      <VCard
+        v-if="page"
+        width="100%"
+        tile
+        flat
+        color="transparent"
+        class="roundedBorder"
+      >
+        <div
+          class="mx-6 text-body-1"
+          style="max-width: 700px"
+          v-html="$translate({ et: page.content_et, en: page.content_en })"
+        />
+      </VCard>
+    </div>
+  </VMain>
+</template>
+
+<style scoped>
+.aboutpage {
+  padding: 20px;
+  /* border-top: solid 1px #6a76ab;
+  border-bottom: solid 1px #6a76ab; */
+}
+
+.aboutpage > div {
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: left;
+}
+</style>
---
+++ pages/institution/elm.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const { data: page } = await useGeoloogiaApiFetch("/web_pages/64/");
+
+const { $translate } = useNuxtApp();
+
+useHead({
+  title: $translate({
+    et: page.title_et,
+    en: page.title_en,
+  }),
+});
+</script>
+
+<template>
+  <VMain>
+    <VContainer style="max-width: 800px" class="mx-auto">
+      <VRow no-gutters>
+        <VCol class="pl-4" cols="12">
+          <BaseHeader
+            class="mb-4"
+            :title="$translate({ et: page.title_et, en: page.title_en })"
+          />
+          <VCard
+            v-if="page"
+            width=""
+            tile
+            flat
+            color="transparent"
+          >
+            <div
+              class="mx-3 aboutpage"
+              v-html="$translate({ et: page.content_et, en: page.content_en })"
+            />
+          </VCard>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </VMain>
+</template>
+
+<style scoped>
+.aboutpage :deep(p) {
+  margin-bottom: 1rem;
+}
+</style>
