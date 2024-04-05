e1d06b48e2b3be1341597dd7cc5959bf6f18ff75
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-04-05 16:38:22 +0300

    fix: change detail title padding on small screens

--- components/HeaderDetailNew.vue
+++ components/HeaderDetailNew.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
     style="word-break: normal; flex-wrap: wrap"
     class="border-b bg-white px-4 w-100"
   >
-    <div class="py-8 px-2">
+    <div class="py-4 py-md-6 px-2 px-md-4">
       <div class="d-flex align-start font-weight-medium text-md-h3 text-h4">
         <slot>
           {{ title }}
