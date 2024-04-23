9d7873c6a54a87e341c3eab482e777a95bf438a6
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-04-23 13:46:11 +0300

    fix: translate text

--- components/base/BaseDataTableExportMenu.vue
+++ components/base/BaseDataTableExportMenu.vue
@@ -1,69 +1,13 @@
 <script setup lang="ts">
 import { mdiFileExportOutline } from "@mdi/js";
-import { utils, writeFileXLSX } from "xlsx";
 import type { ExportSelection } from "~/composables/useExport";
 
 const props = defineProps<{
-  tableElement: HTMLElement;
   exportFunc: ExportFunc;
 }>();
-// function removeSortIndicators(table: HTMLElement) {
-//   const tableCopy = table.cloneNode(true);
-//   // @ts-expect-error
-//   const sortIndicators = tableCopy.querySelectorAll(
-//     "thead > tr > th > .v-data-table-header__sort-badge",
-//   );
-//   sortIndicators.forEach((indicator: any) => {
-//     indicator.parentElement.removeChild(indicator);
-//   });
-//   return tableCopy;
-// }
-// function createWorkbook(table: HTMLElement) {
-//   const tableCopy = removeSortIndicators(table);
-//   const wb = utils.table_to_book(tableCopy);
-//   return wb;
-// }
-// function handleExportCsv() {
-//   try {
-//     const wb = createWorkbook(props.tableElement);
-//
-//     writeFileXLSX(wb, "export.csv", { bookType: "csv" });
-//   }
-//   catch (err) {}
-// }
-// function handleExportExcel() {
-//   try {
-//     const wb = createWorkbook(props.tableElement);
-//
-//     writeFileXLSX(wb, "export.xlsx", { bookType: "xlsx" });
-//   }
-//   catch (err) {}
-// }
-// function handleClipboard() {
-//   const body = document.body;
-//   let range;
-//   let sel: Selection | null;
-//   if (document.createRange && window.getSelection) {
-//     range = document.createRange();
-//     sel = window.getSelection();
-//     sel?.removeAllRanges();
-//     try {
-//       range.selectNodeContents(props.tableElement);
-//       sel?.addRange(range);
-//     }
-//     catch (e) {
-//       range.selectNode(props.tableElement);
-//       sel?.addRange(range);
-//     }
-//     sel?.removeAllRanges();
-//   }
-//   else if ((body as any).createTextRange) {
-//     range = (body as any).createTextRange();
-//     range.moveToElementText(props.tableElement);
-//     range.select();
-//   }
-//   document.execCommand("Copy");
-// }
+
+const { t } = useI18n({ useScope: "local" });
+
 const selection = ref<ExportSelection>("page");
 const exportType = ref<ExportType>("csv");
 const filename = ref("export");
@@ -98,7 +42,7 @@ function handleExport() {
     </template>
     <VCard class="pa-2">
       <div class="text-subtitle-2">
-        Export type
+        {{ t("exportType") }}
       </div>
       <VBtnToggle
         v-model="exportType"
@@ -111,8 +55,8 @@ function handleExport() {
       </VBtnToggle>
       <VSelect
         v-model="selection"
-        :items="[{ title: 'Current page results', value: 'page' }, { title: 'All results', value: 'all' }]"
-        label="Selection"
+        :items="[{ title: t('currentPage'), value: 'page' }, { title: t('allResults'), value: 'all' }]"
+        :label="t('selection')"
         density="compact"
         variant="outlined"
         hide-details
@@ -123,7 +67,7 @@ function handleExport() {
         density="compact"
         hide-details
         variant="outlined"
-        label="Filename"
+        :label="t('filename')"
         :suffix="`.${exportType}`"
       />
       <div class="text-right">
@@ -136,7 +80,7 @@ function handleExport() {
           <VIcon start>
             {{ mdiFileExportOutline }}
           </VIcon>
-          Export
+          {{ t("export") }}
         </VBtn>
       </div>
 
@@ -156,3 +100,20 @@ function handleExport() {
     </VCard>
   </VMenu>
 </template>
+
+<i18n lang="yaml">
+et:
+  export: "Ekspordi"
+  filename: "Failinimi"
+  selection: "Valik"
+  currentPage: "Praeguse lehe tulemused"
+  allResults: "Kõik tulemused"
+  exportType: "Ekspordi tüüp"
+en:
+  export: "Export"
+  filename: "Filename"
+  selection: "Selection"
+  currentPage: "Current page results"
+  allResults: "All results"
+  exportType: "Export type"
+</i18n>
