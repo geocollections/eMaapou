24e7f8157b2daa7311a25aa4c5ae05cc22f18548
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-04-26 11:51:09 +0300

    feat: implement graph parameter selection as a list
    in the future when vuetify reimplements tree view component should use
    that

--- components/chart/options/OptionsParameterTreeView.vue
+++ components/chart/options/OptionsParameterTreeView.vue
@@ -1,36 +1,45 @@
-<script>
+<script setup lang="ts">
 import { mdiChartLineVariant } from "@mdi/js";
 
-export default {
-  name: "OptionsParameterTreeView",
-  props: {
-    parameters: {
-      type: Array,
-      required: true,
-    },
-    initialSelection: {
-      type: Array,
-      default: () => [],
-    },
-  },
-  data() {
-    return {
-      search: "",
-    };
-  },
-  computed: {
-    icons() {
-      return {
-        mdiChartLineVariant,
-      };
-    },
-  },
-  methods: {
-    handleInput(event) {
-      this.$emit("input", event);
-    },
-  },
-};
+const props = defineProps<{
+  parameters: any[];
+  initialSelection: any[];
+}>();
+
+const emit = defineEmits(["input"]);
+
+const search = ref("");
+const searchDebounced = refDebounced(search, 200);
+
+const flatParameters = computed(() => props.parameters.flatMap((p) => {
+  return p.children.flatMap((c) => {
+    return { ...c, method: p.name, method_en: p.name };
+  });
+}));
+
+const { $translate } = useNuxtApp();
+
+const filteredParameters = computed(() => {
+  return flatParameters.value?.filter((p) => {
+    const name = $translate({ et: p.name, en: p.name_en ?? p.name });
+    const method = $translate({ et: p.method, en: p.method_en });
+    const lowercaseSearch = searchDebounced.value.toLowerCase();
+    return name.toLowerCase().includes(lowercaseSearch)
+      || method.toLowerCase().includes(lowercaseSearch);
+  });
+});
+
+function handleInput(event: any) {
+  if (isParameterSelected(event))
+    emit("input", props.initialSelection.filter(p => p.id !== event.id));
+
+  else
+    emit("input", [...props.initialSelection, event]);
+}
+
+function isParameterSelected(parameter: any) {
+  return props.initialSelection.some(p => p.id === parameter.id);
+}
 </script>
 
 <template>
@@ -49,7 +58,7 @@ export default {
         v-bind="props"
       >
         <VIcon start>
-          {{ icons.mdiChartLineVariant }}
+          {{ mdiChartLineVariant }}
         </VIcon>
         {{ $t("common.parameters") }}
       </VBtn>
@@ -66,9 +75,35 @@ export default {
           :label="$t('common.filter')"
           class="py-2"
           density="compact"
-          variant="underlined"
+          variant="outlined"
           hide-details
         />
+        <VVirtualScroll
+          :items="filteredParameters"
+          height="500px"
+          item-height="40px"
+        >
+          <template #default="{ item }">
+            <VListItem
+              @click="handleInput(item)"
+            >
+              <template #prepend="{ isActive }">
+                <VListItemAction start>
+                  <VCheckboxBtn :model-value="isParameterSelected(item)" />
+                </VListItemAction>
+              </template>
+              <div>
+                <span class="text-medium-emphasis font-italic">
+                  ({{ $translate({ et: item.method, en: item.method }) }})
+                </span>
+                {{ $translate({ et: item.name, en: item.name_en ?? item.name }) }}
+                <VChip small>
+                  {{ item.count }}
+                </VChip>
+              </div>
+            </VListItem>
+          </template>
+        </VVirtualScroll>
         <!-- TODO: Reimplement treeview when vuetify 3 adds it -->
         <!-- <v-treeview -->
         <!--   style="max-height: 500px" -->
