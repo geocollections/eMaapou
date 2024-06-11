691d66e18f19644f092c55f787be202c9aa03b02
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-06-11 15:40:40 +0300

    test: refreshSuggestionFilters

---
+++ utils/refreshSuggestionFilters.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it, vi } from "vitest";
+
+describe("refreshSuggestionFilters", () => {
+  it("refreshes all filters", () => {
+    const suggestionRefreshMap = {
+      a: vi.fn(),
+      b: vi.fn(),
+      c: vi.fn(),
+    };
+    const excludeKey = undefined;
+    refreshSuggestionFilters(suggestionRefreshMap, excludeKey);
+    expect(suggestionRefreshMap.a).toHaveBeenCalledTimes(1);
+    expect(suggestionRefreshMap.b).toHaveBeenCalledTimes(1);
+    expect(suggestionRefreshMap.c).toHaveBeenCalledTimes(1);
+  });
+
+  it("excludes filter", () => {
+    const suggestionRefreshMap = {
+      a: vi.fn(),
+      b: vi.fn(),
+      c: vi.fn(),
+    };
+    const excludeKey = "b";
+    refreshSuggestionFilters(suggestionRefreshMap, excludeKey);
+    expect(suggestionRefreshMap.a).toHaveBeenCalledTimes(1);
+    expect(suggestionRefreshMap.b).not.toHaveBeenCalled();
+    expect(suggestionRefreshMap.c).toHaveBeenCalledTimes(1);
+  });
+
+  it("excluded filter does not exist", () => {
+    const suggestionRefreshMap = {
+      a: vi.fn(),
+      b: vi.fn(),
+      c: vi.fn(),
+    };
+    const excludeKey = "d";
+    refreshSuggestionFilters(suggestionRefreshMap, excludeKey);
+    expect(suggestionRefreshMap.a).toHaveBeenCalledTimes(1);
+    expect(suggestionRefreshMap.b).toHaveBeenCalledTimes(1);
+    expect(suggestionRefreshMap.c).toHaveBeenCalledTimes(1);
+  });
+});
