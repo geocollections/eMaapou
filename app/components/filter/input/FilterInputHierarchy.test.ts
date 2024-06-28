d8bd64e2450e7d5c258836a7488a65bf328c13c5
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-06-28 11:07:20 +0300

    test(FilterInputHierarchy): add show more and show children tests

--- app/components/filter/input/FilterInputHierarchy.test.ts
+++ app/components/filter/input/FilterInputHierarchy.test.ts
@@ -163,4 +163,118 @@ describe("filterInputHierarchy", () => {
     const grandChildren = child.findAll("[data-test=\"tree-item\"]");
     expect(grandChildren).toHaveLength(1);
   });
+
+  it("should show more children when show more is clicked", async () => {
+    const wrapper = mount({
+      template: `
+        <Suspense>
+        <VExpansionPanels>
+          <FilterInputHierarchy 
+            v-model=\"modelValue\" 
+            :title=\"title\" 
+            :root-value=\"rootValue\" 
+            :hydration-function=\"hydrationFunction\"
+            :get-children=\"getChildren\"
+            :suggestion-function=\"suggestionFunction\"
+          />
+        </VExpansionPanels>
+        </Suspense>`,
+    }, {
+      components: { FilterInputHierarchy },
+      data() {
+        return {
+          title: "Title",
+          modelValue: [],
+          rootValue: "",
+          hydrationFunction: async () => [],
+          getChildren: async (): Promise<[TreeNode[], number]> => {
+            const child = {
+              id: 1,
+              name: "item1",
+              leaf: false,
+              children: [],
+              childrenLoaded: false,
+              showChildren: false,
+              count: 0,
+              value: "item1",
+            };
+            return [Array.from<TreeNode>({ length: 10 }, _e => ({ ...child })), 20];
+          },
+          suggestionFunction: async () => [],
+        };
+      },
+      global: {
+        plugins: [vuetify, i18n],
+      },
+    });
+
+    await flushPromises();
+    await wrapper.find(titleSelector).trigger("click");
+
+    const rootTreeItem = wrapper.get("[data-test=\"tree-item\"]");
+    await rootTreeItem.get("[data-test=\"show-children\"]").trigger("click");
+
+    const children = wrapper.findAll("[data-test=\"tree-item\"]");
+
+    const child = children[0]!;
+    await child.get("[data-test=\"show-more\"]").trigger("click");
+
+    const grandChildren = child.findAll("[data-test=\"tree-item\"]");
+    expect(grandChildren).toHaveLength(20);
+  });
+
+  it("should hide \"show more\" button when ran out of children", async () => {
+    const wrapper = mount({
+      template: `
+        <Suspense>
+        <VExpansionPanels>
+          <FilterInputHierarchy 
+            v-model=\"modelValue\" 
+            :title=\"title\" 
+            :root-value=\"rootValue\" 
+            :hydration-function=\"hydrationFunction\"
+            :get-children=\"getChildren\"
+            :suggestion-function=\"suggestionFunction\"
+          />
+        </VExpansionPanels>
+        </Suspense>`,
+    }, {
+      components: { FilterInputHierarchy },
+      data() {
+        return {
+          title: "Title",
+          modelValue: [],
+          rootValue: "",
+          hydrationFunction: async () => [],
+          getChildren: async (): Promise<[TreeNode[], number]> => {
+            const child = {
+              id: 1,
+              name: "item1",
+              leaf: false,
+              children: [],
+              childrenLoaded: false,
+              showChildren: false,
+              count: 0,
+              value: "item1",
+            };
+            return [Array.from<TreeNode>({ length: 10 }, _e => ({ ...child })), 20];
+          },
+          suggestionFunction: async () => [],
+        };
+      },
+      global: {
+        plugins: [vuetify, i18n],
+      },
+    });
+
+    await flushPromises();
+    await wrapper.find(titleSelector).trigger("click");
+
+    const rootTreeItem = wrapper.get("[data-test=\"tree-item\"]");
+    await rootTreeItem.get("[data-test=\"show-children\"]").trigger("click");
+    const showMoreButton = rootTreeItem.get("[data-test=\"show-more\"]");
+    await showMoreButton.trigger("click");
+
+    expect(rootTreeItem.find("[data-test=\"show-more\"]").exists()).toBe(false);
+  });
 });
