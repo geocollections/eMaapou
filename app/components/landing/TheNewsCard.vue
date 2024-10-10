a0c79398b36417c061a387de0acb9cd02b6f5f28
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-10-10 16:17:45 +0300

    update
    pls no memory leak

--- app/components/landing/TheNewsCard.vue
+++ app/components/landing/TheNewsCard.vue
@@ -2,44 +2,40 @@
 const { $translate } = useNuxtApp();
 const localePath = useLocalePath();
 
-const { data: newsList } = await useApiFetch<any>("/webnews", {
+const { data: newsList } = await useNewApiFetch<any>("/web-news/", {
   query: {
     limit: 6,
-    order_by: "-date_added",
+    ordering: "-date_added",
   },
 });
 </script>
 
 <template>
-  <VCard flat color="transparent">
-    <VRow no-gutters>
-      <VCol>
-        <BaseHeader :title="$t('common.news')" class="pb-2 title-heading" />
-        <VCardText class="d-flex flex-column pa-0">
-          <NewsPreviewCard
-            v-for="(news, i) in newsList.results"
-            :key="i"
-            class="mb-2"
-            :preview-lenght="400"
-            :date="news.date_added"
-            :title="$translate({ et: news.title_et, en: news.title_en })"
-            :content="$translate({ et: news.text_et, en: news.text_en })"
-            :to="localePath({ name: 'news-id', params: { id: news.id } })"
-          />
-        </VCardText>
-        <VCardActions>
-          <VSpacer />
-          <VBtn
-            nuxt
-            color="accent"
-            class="montserrat text-none"
-            :to="localePath('news')"
-            variant="text"
-          >
-            {{ $t("common.viewNews") }}
-          </VBtn>
-        </VCardActions>
-      </VCol>
-    </VRow>
-  </VCard>
+  <div>
+    <BaseHeader :title="$t('common.news')" class="pb-2 title-heading" />
+    <div class="d-flex flex-column pa-0">
+      <NewsPreviewCard
+        v-for="(news, i) in newsList.results"
+        :key="i"
+        class="mb-2"
+        :preview-lenght="400"
+        :date="news.date_added"
+        :title="$translate({ et: news.title_et, en: news.title_en })"
+        :content="$translate({ et: news.text_et, en: news.text_en })"
+        :to="localePath({ name: 'news-id', params: { id: news.id } })"
+      />
+    </div>
+    <div>
+      <VSpacer />
+      <VBtn
+        nuxt
+        color="accent"
+        class="montserrat text-none"
+        :to="localePath('news')"
+        variant="text"
+      >
+        {{ $t("common.viewNews") }}
+      </VBtn>
+    </div>
+  </div>
 </template>
