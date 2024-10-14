a2bd8db36a219fee665b2a3afed396da01953d4a
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-10-14 13:41:36 +0300

    fix: render menu client-side

--- app/components/base/BaseDataTableExportMenu.vue
+++ app/components/base/BaseDataTableExportMenu.vue
@@ -18,91 +18,95 @@ function handleExport() {
 </script>
 
 <template>
-  <VMenu
-    transition="slide-y-transition"
-    offset="10"
-    position="bottom"
-    width="300"
-    :close-on-content-click="false"
-    z-index="4"
+  <VBtn
+    aria-label="export table"
+    variant="text"
+    class="mr-1"
+    :icon="true"
   >
-    <template #activator="menu">
-      <VTooltip location="bottom" open-delay="500">
-        <template #activator="tooltip">
-          <VBtn
-            aria-label="export table"
-            variant="text"
-            class="mr-1"
-            v-bind="{ ...menu.props, ...tooltip.props }"
-            :icon="mdiFileExportOutline"
-          />
-        </template>
-        <span>{{ $t("table.tooltipExport") }}</span>
-      </VTooltip>
-    </template>
-    <VCard class="pa-2">
-      <div class="text-subtitle-2">
-        {{ t("exportType") }}
-      </div>
-      <VBtnToggle
-        v-model="exportType"
-        class="mb-2"
-        density="compact"
-        mandatory
+    <VIcon>{{ mdiFileExportOutline }}</VIcon>
+    <VTooltip
+      activator="parent"
+      location="bottom"
+      open-delay="500"
+    >
+      <span>{{ $t("table.tooltipExport") }}</span>
+    </VTooltip>
+    <ClientOnly>
+      <VMenu
+        activator="parent"
+        transition="slide-y-transition"
+        offset="10"
+        position="bottom"
+        width="300"
+        :close-on-content-click="false"
+        z-index="4"
       >
-        <VBtn value="csv">
-          CSV
-        </VBtn>
-        <VBtn value="xlsx">
-          XLSX
-        </VBtn>
-      </VBtnToggle>
-      <VSelect
-        v-model="selection"
-        :items="[{ title: t('currentPage'), value: 'page' }, { title: t('allResults'), value: 'all' }]"
-        :label="t('selection')"
-        density="compact"
-        variant="outlined"
-        hide-details
-      />
-      <VTextField
-        v-model="filename"
-        class="py-2"
-        density="compact"
-        hide-details
-        variant="outlined"
-        :label="t('filename')"
-        :suffix="`.${exportType}`"
-      />
-      <div class="text-right">
-        <VBtn
-          flat
-          color="blue"
-          class="text-none"
-          @click="handleExport"
-        >
-          <VIcon start>
-            {{ mdiFileExportOutline }}
-          </VIcon>
-          {{ t("export") }}
-        </VBtn>
-      </div>
+        <VCard class="pa-2">
+          <div class="text-subtitle-2">
+            {{ t("exportType") }}
+          </div>
+          <VBtnToggle
+            v-model="exportType"
+            class="mb-2"
+            density="compact"
+            mandatory
+          >
+            <VBtn value="csv">
+              CSV
+            </VBtn>
+            <VBtn value="xlsx">
+              XLSX
+            </VBtn>
+          </VBtnToggle>
+          <VSelect
+            v-model="selection"
+            :items="[{ title: t('currentPage'), value: 'page' }, { title: t('allResults'), value: 'all' }]"
+            :label="t('selection')"
+            density="compact"
+            variant="outlined"
+            hide-details
+          />
+          <VTextField
+            v-model="filename"
+            class="py-2"
+            density="compact"
+            hide-details
+            variant="outlined"
+            :label="t('filename')"
+            :suffix="`.${exportType}`"
+          />
+          <div class="text-right">
+            <VBtn
+              flat
+              color="blue"
+              class="text-none"
+              @click="handleExport"
+            >
+              <VIcon start>
+                {{ mdiFileExportOutline }}
+              </VIcon>
+              {{ t("export") }}
+            </VBtn>
+          </div>
 
-      <!-- <VList> -->
-      <!--   <VListItem @click="handleExportCsv"> -->
-      <!--     <VListItemTitle>CSV</VListItemTitle> -->
-      <!--   </VListItem> -->
-      <!-- <VListItem @click="handleExportExcel"> -->
-      <!--   <VListItemTitle>XLSX (Excel)</VListItemTitle> -->
-      <!-- </VListItem> -->
-      <!-- <VListItem @click="handleClipboard"> -->
-      <!--   <VListItemTitle> -->
-      <!--     {{ $t("common.clipboard") }} -->
-      <!--   </VListItemTitle> -->
-      <!-- </VListItem> -->
-      <!-- </VList> -->
-    </VCard>
-  </VMenu>
+        <!-- <VList> -->
+        <!--   <VListItem @click="handleExportCsv"> -->
+        <!--     <VListItemTitle>CSV</VListItemTitle> -->
+        <!--   </VListItem> -->
+        <!-- <VListItem @click="handleExportExcel"> -->
+        <!--   <VListItemTitle>XLSX (Excel)</VListItemTitle> -->
+        <!-- </VListItem> -->
+        <!-- <VListItem @click="handleClipboard"> -->
+        <!--   <VListItemTitle> -->
+        <!--     {{ $t("common.clipboard") }} -->
+        <!--   </VListItemTitle> -->
+        <!-- </VListItem> -->
+        <!-- </VList> -->
+        </VCard>
+      </VMenu>
+    </ClientOnly>
+  </VBtn>
 </template>
 
 <i18n lang="yaml">
