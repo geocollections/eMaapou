b613a3617b05f1f04922c20021266f68716543df
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2025-11-23 13:21:31 +0200

    fix(news): update style and use date_published
    refs: #1159

--- app/pages/news/[id].vue
+++ app/pages/news/[id].vue
@@ -16,11 +16,11 @@ useHead({
 
 <template>
   <VMain>
-    <VContainer style="max-width: 800px">
+    <VContainer style="max-width: 800px; min-height: 100vh">
       <VRow no-gutters>
         <VCol class="my-4">
           <div class="text-right montserrat pr-4 text--secondary">
-            {{ $formatDate(news.date_added) }}
+            {{ $formatDate(news.date_published) }}
           </div>
           <div class="text-h4">
             {{ $translate({ et: news.title_et, en: news.title_en }) }}
@@ -30,17 +30,22 @@ useHead({
       <VRow no-gutters justify="center">
         <VCol>
           <div
-            class="text-body-1"
+            class="text-body-1 news-text"
             v-html="$translate({ et: news.text_et, en: news.text_en })"
           />
         </VCol>
       </VRow>
     </VContainer>
+    <AppFooter />
   </VMain>
 </template>
 
 <style scoped>
 :deep(p) {
   margin-bottom: 16px
+}
+
+.news-text {
+  list-style-position: inside;
 }
 </style>
